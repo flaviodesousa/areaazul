b2405d6cdf3c7dc5cbc36757b82d24df7c3a6552
tiago alan rezende <tiago@debian.desenvolvimento.debian.com.br>
2014-07-14 11:45:29 -0300

    correcoes para require

--- models/collections/configuracao.js
+++ models/collections/configuracao.js
@@ -1,5 +1,5 @@
 var Bookshelf = require('bookshelf').conexaoMain;
-var Configuracao = require("./models/models/configuracao");
+var Configuracao = require("/models/models/configuracao");
 
 module.export = Bookshelf.Collection.extend({
     model: Configuracao.Configuracao
