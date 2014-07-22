c936dcb73f256460575115d9f052e2f9cdb936ee
jefferson souza e silva <jefferson@debian.desenvolvimento.debian.com.br>
2014-07-22 10:07:34 -0300

    Correção função cadastrar

--- models/models/usuario.js
+++ models/models/usuario.js
@@ -51,3 +51,63 @@ exports.validate = function(user) {
     }
     return true;
 }
+
+exports.cadastrar = function(user) {
+    var usuario = new Usuario.Usuario({
+        'nome': 'usuario_nome',
+        'senha': 'usuario_senha',
+        'autorizacao': '1'
+    });
+    var pessoa = new Pessoa.Pessoa({
+        nome: req.param('nome'),
+        email: req.param('email'),
+        telefone: req.param('telefone')
+    });
+    var pessoa_fisica = new PessoaFisica.PessoaFisica({
+        cpf: req.param('cpf'),
+        data_nascimento: req.param('data_nascimento')
+    });
+
+    var erro = false;
+
+    if (Usuario.validate(usuario) == true && Pessoa.validate(pessoa) == true && PessoaFisica.validate(pessoa_fisica) == true) {
+        Bookshelf.transaction(function(t) {
+            pessoa.save(null, {
+                transacting: t
+            }).then(function(model, err) {
+                usuario.attributes.pessoa_id = model.id;
+                pessoa_fisica.attributes.pessoa_id = model.id;
+                usuario.save(null, {
+                    transacting: t
+                }).then(function(model, err) {
+                    pessoa_fisica.save(null, {
+                        transacting: t
+                    }).then(function(model, err) {
+                        res.statusCode = 201;
+                        t.commit();
+                    }).
+                    catch(function(err) {
+                        console.log(err);
+                        res.statusCode = 500;
+                        t.rollback();
+                    });
+                }).
+                catch(function(err) {
+
+                    console.log(err);
+                    res.statusCode = 500;
+                    t.rollback();
+                });
+            }).
+            catch(function(err) {
+                t.rollback();
+                console.log(err);
+                res.statusCode = 500;
+            });
+        });
+        res.json();
+        console.log("Ocorreu tudo certo");
+    } else {
+        console.log("Erros");
+    }
+}
