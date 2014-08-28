7feffca6256da129f7a8bf808256b26d503fea90
jefferson souza e silva <jefferson@debian.desenvolvimento.debian.com.br>
2014-08-28 09:31:48 -0300

    Alteração modelo veiculo função cadastrar

--- models/models/veiculo.js
+++ models/models/veiculo.js
@@ -11,7 +11,7 @@ exports.cadastrar = function(vehicle, fail, then){
 
   
   var vehicle = new this.Veiculo({
-       'estado_id': vehicle.id_estado,
+       'estado_id': vehicle.estado_id,
        'placa': vehicle.placa,
        'placa_numero': vehicle.placa_numero,
        'marca': vehicle.marca,
