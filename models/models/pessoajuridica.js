fe702e653c8f2e9e98307f0d3401b10fca56a54a
jefferson souza e silva <jefferson@debian.desenvolvimento.debian.com.br>
2014-09-24 13:43:43 -0300

    Commit alterações rotas

--- models/models/pessoajuridica.js
+++ models/models/pessoajuridica.js
@@ -1,6 +1,10 @@
 var Bookshelf = require('bookshelf').conexaoMain;
 var validator = require('validator');
 
+var validation = require('./validation');
+
+
+
 var PessoaJuridica = Bookshelf.Model.extend({
     tableName: 'pessoa_juridica',
     idAttribute: 'id_pessoa_juridica'
@@ -10,17 +14,22 @@ var PessoaJuridica = Bookshelf.Model.extend({
 exports.validate = function(pessoaJuridica){
 
    if (validator.isNull(pessoaJuridica.attributes.cnpj) == true || pessoaJuridica.attributes.cnpj == '') {
-        console.log("Cnpj obrigatório");
+        console.log("Cnpj obrigatório!");
+        return false;
+    }
+
+    if(validation.isCNPJ(pessoaJuridica.attributes.cnpj) != true){
+        console.log("Cnpj inválido!");
         return false;
     }
 
    if (validator.isNull(pessoaJuridica.attributes.razao_social) == true || pessoaJuridica.attributes.razao_social == '') {
-        console.log("Cnpj razao social");
+        console.log("Cnpj razao social!");
         return false;
     }
 
    if (validator.isNull(pessoaJuridica.attributes.contato) == true || pessoaJuridica.attributes.contato == '') {
-        console.log("Contato obrigatório");
+        console.log("Contato obrigatório!");
         return false;
     }
     return true;
