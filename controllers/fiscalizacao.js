5d7e2f66d5b3739a1cfd547e71bc33b2c7edcbee
Flavio de Sousa <fl@viodesousa.com>
2015-01-16 01:23:17 +0100

    Servico fiscalizacao retornando status 200

--- controllers/fiscalizacao.js
+++ controllers/fiscalizacao.js
@@ -1,7 +1,9 @@
 module.exports = function(app) {
     return {
         registrar: function(req, res) {
-
+        	console.log('body');
+        	console.log(req.body);
+        	res.status(200).end();
         }
     }
 }
