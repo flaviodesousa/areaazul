b7193d2ed29f89e9d696cdfb73e6478ffec3479a
Flavio de Sousa <fl@viodesousa.com>
2015-01-17 22:39:49 +0100

    Logging body contents

--- controllers/fiscalizacao.js
+++ controllers/fiscalizacao.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
     return {
         registrar: function(req, res) {
         	console.log('body');
-        	console.log(req.body);
+        	console.dir(req.body);
         	res.status(200).end();
         }
     }
