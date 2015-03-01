e10dee7db34fafdc60d65391840f87a3bc0b3829
Flavio de Sousa <fl@viodesousa.com>
2015-03-01 20:51:33 +0100

    fiscalizacao: chamada de persistencia

--- controllers/fiscalizacao.js
+++ controllers/fiscalizacao.js
@@ -3,7 +3,20 @@ module.exports = function(app) {
         registrar: function(req, res) {
         	console.log('body');
         	console.dir(req.body);
-        	res.status(200).end();
+            Fiscalizacao.cadastrar({
+            	fiscal: req.body.fiscal,
+  				senha: req.body.senha,
+  				placa: req.body.placa,
+  				latitude: req.body.latitude,
+  				longitude: req.body.longitude },
+                function(result) {
+                    console.log("Cadastrado com sucesso!!!");
+                    res.status(200).end();
+                },
+                function(result) {
+                    console.log("Erro ao salvar!!!");
+                    res.status(400).end();
+                })
         }
     }
 }
