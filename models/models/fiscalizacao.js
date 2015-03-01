44d18c0e618d4c40ef091141de1ca41a16ef81cb
Flavio de Sousa <fl@viodesousa.com>
2015-03-01 20:50:39 +0100

    cadastrar/listar methods for fiscalizacao

--- models/models/fiscalizacao.js
+++ models/models/fiscalizacao.js
@@ -5,4 +5,31 @@ var Fiscalizacao = Bookshelf.Model.extend({
     idAttribute: 'id_fiscalizacao'
 });
 
+exports.cadastrar = function(fiscalizacao_params, then, fail) {
+	var fiscalizacao = new this.Fiscalizacao({
+		placa: fiscalizacao_params.placa,
+		latitude: fiscalizacao_params.latitude,
+		longitude: fiscalizacao_params.longitude,
+		timestamp: new Date(),
+		fiscal_id: 1
+	});
+	fiscalizacao.save().then(function(model, err)) {
+		if (err) {
+			fail(false);
+		} else {
+			then(true);
+		}
+	}
+}
+
+exports.listar = function(consulta_fiscalizacao_params, then, fail) {
+	Bookshelf.Collection.extend({
+		model: this.Fiscalizacao
+	}).forge().query(function(qb) {
+		qb.select('fiscalizacao.*')
+	}).fetch().then(function(collection) {
+		then(collection);
+	})
+}
+
 exports.Fiscalizacao = Fiscalizacao;
