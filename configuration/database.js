71e91be77363cceb74bb4b26298014b6f044442d
Flavio de Sousa <fl@viodesousa.com>
2015-04-23 19:50:16 +0200

    Atribuicao de versao correta em conexaoMain

--- configuration/database.js
+++ configuration/database.js
@@ -5,5 +5,5 @@ var env = process.env.NODE_ENV || 'development';
 var knex = require('knex')(knexfile[env]);
 knex.migrate.latest();
 
-export.Bookshelf = require('bookshelf')(knex);
-export.Bookshelf.conexaoMain = export.Bookshelf;
+exports.Bookshelf = require('bookshelf');
+exports.Bookshelf.conexaoMain = require('bookshelf')(knex);
