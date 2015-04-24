e4b1d29b9c5d66552884b3ef9183f3e4937f5a5c
Flavio de Sousa <fl@viodesousa.com>
2015-04-24 19:19:06 +0200

    Diretorio relativo para migrations

--- configuration/database.js
+++ configuration/database.js
@@ -3,6 +3,9 @@ var knexfile = require('../knexfile');
 var env = process.env.NODE_ENV || 'development';
 
 var knex = require('knex')(knexfile[env]);
+var migrations_config = { directory: path.join(__dirname, '..', 'migrations'}
+console.log('migrations_config: ');
+console.dir(migrations_config);
 knex.migrate.latest();
 
 exports.Bookshelf = require('bookshelf');
