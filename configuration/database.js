9cc0938df582eb3d0ad3f89e55d5ee8e613b28d8
Flavio de Sousa <fl@viodesousa.com>
2015-04-24 19:21:16 +0200

    Proper use of path.join()

--- configuration/database.js
+++ configuration/database.js
@@ -1,9 +1,12 @@
+var path = require('path');
 var knexfile = require('../knexfile');
 
 var env = process.env.NODE_ENV || 'development';
 
 var knex = require('knex')(knexfile[env]);
-var migrations_config = { directory: path.join(__dirname, '..', 'migrations'}
+var migrations_config = {
+    directory: path.join(__dirname, '..', 'migrations')
+};
 console.log('migrations_config: ');
 console.dir(migrations_config);
 knex.migrate.latest();
