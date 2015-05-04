5d8bdb588d707d6f761175eb8caf871d567809be
Flavio de Sousa <fl@viodesousa.com>
2015-05-04 09:16:32 +0200

    Preparando para migracoes sincronas

--- configuration/database.js
+++ configuration/database.js
@@ -4,10 +4,9 @@ var knexfile = require('../knexfile');
 var env = process.env.NODE_ENV || 'development';
 
 var knex = require('knex')(knexfile[env]);
-var migrations_config = {
+knex.migrate.latest({
   directory: path.join(__dirname, '..', 'migrations')
-};
-knex.migrate.latest(migrations_config).thenReturn();
+});
 
 exports.Bookshelf = require('bookshelf');
 exports.Bookshelf.conexaoMain = require('bookshelf')(knex);
