99e5cd321e0beac5252c37da9e21a2b5caf4aa8a
Flavio de Sousa <fl@viodesousa.com>
2015-05-12 14:25:03 +0200

    Remocao da coluna autorizacao

--- migrations/20150422195611_usuario.js
+++ migrations/20150422195611_usuario.js
@@ -1,18 +1,17 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('usuario', function(table) {
-        table.bigInteger('pessoa_id')
-            .primary()
-            .references('pessoa_id').inTable('pessoa_fisica');
-        table.string('login').unique().notNullable();
-        table.string('senha');
-        table.integer('autorizacao').notNullable();
-        table.boolean('primeiro_acesso').notNullable();
-        table.boolean('ativo').notNullable().defaultTo(true);
-    });
+exports.up = function(knex) {
+  return knex.schema.createTable('usuario', function(table) {
+    table.bigInteger('pessoa_id')
+        .primary()
+        .references('pessoa_id').inTable('pessoa_fisica');
+    table.string('login').unique().notNullable();
+    table.string('senha');
+    table.boolean('primeiro_acesso').notNullable();
+    table.boolean('ativo').notNullable().defaultTo(true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-	return knex.schema.dropTable('usuario');
+exports.down = function(knex) {
+  return knex.schema.dropTable('usuario');
 };
