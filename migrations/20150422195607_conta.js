6352b97339c08e2918b6af4fdf6ec9f4a4643588
Flavio de Sousa <fl@viodesousa.com>
2015-05-13 11:46:48 +0200

    Defaults para saldo e ativo (+ JSCS)

--- migrations/20150422195607_conta.js
+++ migrations/20150422195607_conta.js
@@ -1,16 +1,17 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
-    return knex.schema.createTable('conta', function(table) {
-        table.increments('id_conta').primary();
-        table.timestamp('data_abertura').notNullable();
-        table.timestamp('data_fechamento');
-        table.decimal('saldo',18,2).notNullable();
-        table.boolean('ativo').notNullable();
-        table.bigInteger('pessoa_id').notNullable().references('id_pessoa').inTable('pessoa');
-    });
+exports.up = function(knex) {
+  return knex.schema.createTable('conta', function(table) {
+    table.increments('id_conta').primary();
+    table.timestamp('data_abertura').notNullable();
+    table.timestamp('data_fechamento');
+    table.decimal('saldo', 18, 2).notNullable().defaultTo(0);
+    table.boolean('ativo').notNullable().defaultTo(true);
+    table.bigInteger('pessoa_id').notNullable()
+      .references('id_pessoa').inTable('pessoa');
+  });
 };
 
-exports.down = function(knex, Promise) {
-	return knex.schema.dropTable('conta');
+exports.down = function(knex) {
+  return knex.schema.dropTable('conta');
 };
