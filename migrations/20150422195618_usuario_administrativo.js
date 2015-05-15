434d5b97bd8f0d60937ab5fc87fe1cf5ced928e5
Flavio de Sousa <fl@viodesousa.com>
2015-05-15 11:17:37 +0200

    JSCS + coluna autorizacao (necessaria em admin)

--- migrations/20150422195618_usuario_administrativo.js
+++ migrations/20150422195618_usuario_administrativo.js
@@ -1,18 +1,19 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
-    return knex.schema
-        .createTable('usuario_administrativo', function(table) {
-            table.bigInteger('pessoa_fisica_pessoa_id')
-            	.primary()
-            	.references('pessoa_id').inTable('pessoa_fisica');
-            table.string('login').unique().notNullable();
-            table.string('senha');
-            table.boolean('primeiro_acesso').notNullable();
-            table.boolean('ativo').notNullable();
-        });
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('usuario_administrativo', function(table) {
+      table.bigInteger('pessoa_fisica_pessoa_id')
+          .primary()
+          .references('pessoa_id').inTable('pessoa_fisica');
+      table.string('login').unique().notNullable();
+      table.string('senha');
+      table.string('autorizacao');
+      table.boolean('primeiro_acesso').notNullable();
+      table.boolean('ativo').notNullable().defaultTo(true);
+    });
 };
 
-exports.down = function(knex, Promise) {
-	return knex.schema.dropTable('usuario_administrativo');
+exports.down = function(knex) {
+  return knex.schema.dropTable('usuario_administrativo');
 };
