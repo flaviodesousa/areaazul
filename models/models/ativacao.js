7735ee56a1b712013480f2d0d834dacd49b08f71
Flavio de Sousa <fl@viodesousa.com>
2015-05-17 01:13:17 +0200

    JSCS

--- models/models/ativacao.js
+++ models/models/ativacao.js
@@ -1,85 +1,75 @@
+'use strict';
+
+var validator = require('validator');
 var Bookshelf = require('bookshelf').conexaoMain;
-var Veiculo = require('./veiculo');
 var Usuario = require('./usuario');
-var util = require('./util');
 var Revendedor = require('./revendedor');
 
-
-
 var Ativacao = Bookshelf.Model.extend({
-    tableName: 'ativacao',
-    idAttribute: 'id_ativacao'
+  tableName: 'ativacao',
+  idAttribute: 'id_ativacao',
 });
 
 exports.Ativacao = Ativacao;
 
+exports.ativar = function(activation, then, fail) {
 
-exports.ativar = function(activation, then, fail){
+  var latitude = activation.latitude;
+  var altitude = activation.longitude;
+  var longitude = activation.altitude;
 
-    var latitude = activation.latitude;
-    var altitude = activation.longitude;
-    var longitude = activation.altitude;
+  if (validator.isNull(latitude)) {
+    latitude = null;
+  }
+  if (validator.isNull(longitude)) {
+    longitude = null;
+  }
+  if (validator.isNull(altitude)) {
+    altitude = 0;
+  }
 
-    if(latitude == ""){
-        latitude = 0;
-    } 
-    if(longitude == ""){
-        longitude = 0;
-    }
-    if(altitude == ""){
-        altitude = 0;
-    }
+  var ativacao = new this.Ativacao({
+    data_ativacao: new Date(),
+    latitude: latitude,
+    longitude: longitude,
+    altitude: altitude,
+    usuario_id: activation.usuario_id,
+    veiculo_id: activation.veiculo_id,
+    ativo: true,
+  });
 
-    var ativacao = new this.Ativacao({
-         'data_ativacao': new Date(),
-         'latitude': latitude,
-         'longitude': longitude,
-         'altitude': altitude,
-         'usuario_id': activation.usuario_id,
-         'veiculo_id': activation.veiculo_id,
-         'ativo': 'true'
-    });
+  ativacao.save().then(function(model) {
+    then(model);
+  }).catch(function(err) {
+    fail(err);
+  });
 
+};
 
-    ativacao.save().then(function(model) { 
-        then(model);
-    }).catch(function(err) {
-        fail(err);
-    });
+exports.ativarPelaRevenda = function(car, then, fail) {
 
+  var ativacao = new this.Ativacao({
+    data_ativacao: new Date(),
+    usuario_id: car.usuario_id,
+    veiculo_id: car.veiculo_id,
+    ativo: true,
+  });
 
-}
+  var usuario = new Usuario.Usuario({
+    id_usuario: car.usuario_id,
+  });
 
-exports.ativarPelaRevenda = function(car, then, fail){
-
-    console.log("usuario"+car.usuario_id);
-     console.log("veiculo"+car.veiculo_id);
-    var ativacao = new this.Ativacao({
-         'data_ativacao': new Date(),
-         'usuario_id': car.usuario_id,
-         'veiculo_id': car.veiculo_id,
-         'ativo': 'true'  
-    });
-
-    var usuario = new Usuario.Usuario({
-        'id_usuario': car.usuario_id
-    });
-
-    console.log(usuario);
-
-    Revendedor.buscarRevendedor(usuario,
-        function(result){
-            ativacao.save().then(function(model) {
-                console.log("sucesso");
-                then(model);
-            }).catch(function(err) {
-                console.log("erro"+err);
-                fail(err);
-            });
-        }, 
-        function(result){
-            fail(result);
+  Revendedor.buscarRevendedor(usuario,
+        function() {
+          ativacao.save().then(function(model) {
+            then(model);
+          }).catch(function(err) {
+            fail(err);
+          });
+        },
+        function(result) {
+          fail(result);
         }
     );
 
-}
+};
--- models/models/usuario_has_veiculo.js
+++ models/models/usuario_has_veiculo.js
@@ -1,9 +1,8 @@
 var Bookshelf = require('bookshelf').conexaoMain;
 
-var Usuario_has_Veiculo = Bookshelf.Model.extend({
-    tableName: 'usuario_has_veiculo',
-    idAttribute: 'usuario_id',
-    idAttribute: 'veiculo_id'
+var UsuarioHasVeiculo = Bookshelf.Model.extend({
+  tableName: 'usuario_has_veiculo',
+  idAttribute: ['usuario_id', 'veiculo_id'],
 });
 
-exports.Usuario_has_Veiculo = Usuario_has_Veiculo;
+module.exports = UsuarioHasVeiculo;
