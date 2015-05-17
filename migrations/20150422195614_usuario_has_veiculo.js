b17744e4b9338c6b4f68f0135f2a6f7fd63595a0
Flavio de Sousa <fl@viodesousa.com>
2015-05-17 14:26:12 +0200

    Relacionamentos obrigatorios em UsuarioHasVeiculo

--- migrations/20150422195614_usuario_has_veiculo.js
+++ migrations/20150422195614_usuario_has_veiculo.js
@@ -1,15 +1,17 @@
 'use strict';
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.createTable('usuario_has_veiculo', function(table) {
-      table.primary(['usuario_pessoa_id','veiculo_id']);
-      table.bigInteger('usuario_pessoa_id')
-      	.references('pessoa_id').inTable('usuario');
-      table.bigInteger('veiculo_id')
-      	.references('id_veiculo').inTable('veiculo');
+    table.primary(['usuario_pessoa_id', 'veiculo_id']);
+    table.bigInteger('usuario_pessoa_id')
+      .notNullable()
+      .references('pessoa_id').inTable('usuario');
+    table.bigInteger('veiculo_id')
+      .notNullable()
+      .references('id_veiculo').inTable('veiculo');
   });
 };
 
-exports.down = function(knex, Promise) {
-	return knex.schema.dropTable('usuario_has_veiculo');
+exports.down = function(knex) {
+  return knex.schema.dropTable('usuario_has_veiculo');
 };
