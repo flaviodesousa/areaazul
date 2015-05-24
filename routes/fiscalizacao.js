4e5aacfaa5b68d1e31a394398bf892f286f4ae1a
Flavio de Sousa <fl@viodesousa.com>
2015-05-24 19:58:00 +0200

    JSCS + JSHint

--- routes/fiscalizacao.js
+++ routes/fiscalizacao.js
@@ -1,10 +1,12 @@
+'use strict';
+
 var passport = require('passport');
 
 module.exports = function(app) {
-    app.post("/fiscalizacao",
-      passport.authenticate('basic-fiscal', { session: false }),
-      app.controllers.fiscalizacao.registrar);
-    app.get('/fiscalizacao',
-      passport.authenticate('basic-fiscal', { session: false }),
-      app.controllers.fiscalizacao.listar);
-}
+  app.post('/fiscalizacao',
+    passport.authenticate('basic-fiscal', { session: false }),
+    app.controllers.fiscalizacao.registrar);
+  app.get('/fiscalizacao',
+    passport.authenticate('basic-fiscal', { session: false }),
+    app.controllers.fiscalizacao.listar);
+};
