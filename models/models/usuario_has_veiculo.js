73a6cd1c11f62cb6fa48d1984239df322a385ca6
Flavio de Sousa <fl@viodesousa.com>
2015-05-26 23:24:18 +0200

    Prorrogacao de dependencia ciclica
    Veiculo depende de UsuarioHasVeiculo
    UsuarioHasVeiculo depende de Veiculo para definir o
    relacionamento. A dependencia foi movida para dentro
    do metodo de relacionamento.

--- models/models/usuario_has_veiculo.js
+++ models/models/usuario_has_veiculo.js
@@ -1,18 +1,14 @@
 'use strict';
 
 var Bookshelf = require('bookshelf').conexaoMain;
-var Veiculo = require('./veiculo').Veiculo;
-var Usuario = require('./usuario').Usuario;
 
-var UsuarioHasVeiculo = Bookshelf.Model.extend({
+module.exports = Bookshelf.Model.extend({
   tableName: 'usuario_has_veiculo',
   idAttribute: ['usuario_pessoa_id', 'veiculo_id'],
   veiculo: function() {
-    return this.belongsTo(Veiculo, 'veiculo_id');
+    return this.belongsTo(require('./veiculo'), 'veiculo_id');
   },
   usuario: function() {
-    return this.belongsTo(Usuario, 'usuario_pessoa_id');
+    return this.belongsTo(require('./usuario'), 'usuario_pessoa_id');
   },
 });
-
-module.exports = UsuarioHasVeiculo;
