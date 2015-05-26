9edeb3d79bfd477503d7e2fa2cda4846b5504b73
jeffersonarar <jeffersonarar@hotmail.com>
2015-05-26 10:20:11 -0300

    Mudança da aboragem do método ativar

--- models/models/ativacao.js
+++ models/models/ativacao.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var validator = require('validator');
 var Bookshelf = require('bookshelf').conexaoMain;
 var Usuario = require('./usuario');
@@ -9,7 +10,9 @@ var Ativacao = Bookshelf.Model.extend({
   tableName: 'ativacao',
   idAttribute: 'id_ativacao',
 
-ativar: function(activation, then, fail) {
+},{
+
+ativar: function(activation) {
 
   var latitude = activation.latitude;
   var altitude = activation.longitude;
@@ -25,24 +28,32 @@ ativar: function(activation, then, fail) {
     altitude = null;
   }
 
-  var ativacao = new this.Ativacao({
-    data_ativacao: new Date(),
-    latitude: latitude,
-    longitude: longitude,
-    altitude: altitude,
-    usuario_id: activation.usuario_id,
-    veiculo_id: activation.veiculo_id,
-    ativo: true,
-  });
+  return Bookshelf.transaction(function(t) {
+      var options = { transacting: t };
+      var optionsInsert = _.merge(options, { method: 'insert' });
+     
+      return Ativacao
+        .forge({
+            data_ativacao: new Date(),
+            latitude: latitude,
+            longitude: longitude,
+            altitude: altitude,
+            usuario_pessoa_id: activation.usuario_pessoa_id,
+            veiculo_id: activation.veiculo_id,
+            ativo: true,
+        })
+        .save(null, optionsInsert)
+        .then(function(ativacao){
+        
+        return ativacao;
+      });
+});
 
-  ativacao.save().then(function(model) {
-    then(model);
-  }).catch(function(err) {
-    fail(err);
-  });
+},
 
-}
-},{
+
+
+
 ativarPelaRevenda: function(car, then, fail) {
 
   var ativacao = new this.Ativacao({
