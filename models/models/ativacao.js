b8279a03870310c93ba7b3745bc1109b464dba06
Flavio de Sousa <fl@viodesousa.com>
2015-05-27 23:16:49 +0200

    JSCS + JSHint

--- models/models/ativacao.js
+++ models/models/ativacao.js
@@ -10,75 +10,73 @@ var Ativacao = Bookshelf.Model.extend({
   tableName: 'ativacao',
   idAttribute: 'id_ativacao',
 
-},{
+}, {
 
-ativar: function(activation) {
+  ativar: function(activation) {
 
-  var latitude = activation.latitude;
-  var altitude = activation.longitude;
-  var longitude = activation.altitude;
+    var latitude = activation.latitude;
+    var altitude = activation.longitude;
+    var longitude = activation.altitude;
 
-  if (validator.isNull(latitude)) {
-    latitude = null;
-  }
-  if (validator.isNull(longitude)) {
-    longitude = null;
-  }
-  if (validator.isNull(altitude)) {
-    altitude = null;
-  }
+    if (validator.isNull(latitude)) {
+      latitude = null;
+    }
+    if (validator.isNull(longitude)) {
+      longitude = null;
+    }
+    if (validator.isNull(altitude)) {
+      altitude = null;
+    }
 
-  return Bookshelf.transaction(function(t) {
+    return Bookshelf.transaction(function(t) {
       var options = { transacting: t };
       var optionsInsert = _.merge(options, { method: 'insert' });
-     
+
       return Ativacao
         .forge({
-            data_ativacao: new Date(),
-            latitude: latitude,
-            longitude: longitude,
-            altitude: altitude,
-            usuario_pessoa_id: activation.usuario_pessoa_id,
-            veiculo_id: activation.veiculo_id,
-            ativo: true,
+          data_ativacao: new Date(),
+          latitude: latitude,
+          longitude: longitude,
+          altitude: altitude,
+          usuario_pessoa_id: activation.usuario_pessoa_id,
+          veiculo_id: activation.veiculo_id,
+          ativo: true,
         })
         .save(null, optionsInsert)
-        .then(function(ativacao){
-        
-        return ativacao;
-      });
-});
+        .then(function(ativacao) {
 
-},
+          return ativacao;
+        });
+    });
 
+  },
 
+  ativarPelaRevenda: function(car, then, fail) {
 
+    var ativacao = new this.Ativacao({
+      data_ativacao: new Date(),
+      usuario_id: car.usuario_id,
+      veiculo_id: car.veiculo_id,
+      ativo: true,
+    });
 
-ativarPelaRevenda: function(car, then, fail) {
+    var usuario = new Usuario.Usuario({
+      id_usuario: car.usuario_id,
+    });
 
-  var ativacao = new this.Ativacao({
-    data_ativacao: new Date(),
-    usuario_id: car.usuario_id,
-    veiculo_id: car.veiculo_id,
-    ativo: true,
-  });
-
-  var usuario = new Usuario.Usuario({
-    id_usuario: car.usuario_id,
-  });
-
-  Revendedor.buscarRevendedor(usuario,
-        function() {
-          ativacao.save().then(function(model) {
-            then(model);
-          }).catch(function(err) {
-            fail(err);
-          });
-        },
-        function(result) {
-          fail(result);
-  });
-}
+    Revendedor.buscarRevendedor(usuario,
+      function() {
+        ativacao.save().then(function(model) {
+          then(model);
+        }).catch(function(err) {
+          fail(err);
+        });
+      },
+      function(result) {
+        fail(result);
+      }
+    );
+  },
 
 });
 
