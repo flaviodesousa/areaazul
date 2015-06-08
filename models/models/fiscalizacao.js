6a14685d71c99c8a881a308d406550e542d9ac9d
Flavio de Sousa <fl@viodesousa.com>
2015-06-08 21:34:17 +0200

    JSCS-Formatter, log de sucesso com objeto model

--- models/models/fiscalizacao.js
+++ models/models/fiscalizacao.js
@@ -1,29 +1,30 @@
+'use strict';
+
 var AreaAzul = require('../../areaazul');
 var log = AreaAzul.log;
 var Bookshelf = require('bookshelf').conexaoMain;
-var moment = require('moment');
 
 module.exports = Bookshelf.Model.extend({
   tableName: 'fiscalizacao',
-  idAttribute: 'id_fiscalizacao'
+  idAttribute: 'id_fiscalizacao',
 }, {
-	cadastrar: function (params, then, fail) {
-		this
-		.forge({
-			placa: params.placa,
-			latitude: params.latitude,
-			longitude: params.longitude,
-			timestamp: new Date(),
-			fiscal_id: params.fiscal_id
-		})
-		.save()
-		.then(function(model) {
-			log.info('Fiscalizacao id: ' + model.id);
-			then(model);
-		})
-		.catch(function(err) {
-			log.error('Fiscalizacao', {params: params, err: err});
-			fail(err);
-		});
-	}
+  cadastrar: function(params, then, fail) {
+    this
+  .forge({
+    placa: params.placa,
+    latitude: params.latitude,
+    longitude: params.longitude,
+    timestamp: new Date(),
+    fiscal_id: params.fiscal_id,
+  })
+  .save()
+  .then(function(model) {
+    log.info('Fiscalizacao adicionada', model);
+    then(model);
+  })
+  .catch(function(err) {
+    log.error('Fiscalizacao', {params: params, err: err});
+    fail(err);
+  });
+  },
 });
