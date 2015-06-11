8a894a189a5d17b4e2abeaa2c0fb8a460f8c172a
jeffersonarar <jeffersonarar@hotmail.com>
2015-06-11 12:49:55 -0300

    Implementação método de procura de veiculos.

--- models/collections/veiculo.js
+++ models/collections/veiculo.js
@@ -2,5 +2,21 @@ var Bookshelf = require('bookshelf').conexaoMain;
 var Veiculo = require("../models/veiculo");
 
 module.exports = Bookshelf.Collection.extend({
-    model: Veiculo.Veiculo
+    model: Veiculo,
+
+},{
+
+procurar: function(vehicle, then, fail){
+        Veiculo.forge().query(function(qb) {
+            qb.where('veiculo.id_veiculo', vehicle.id_veiculo);
+            qb.join('estado', 'estado.id_estado', '=', 'veiculo.estado_id');
+            qb.select('veiculo.*');
+            qb.select('estado.*');
+        }).fetch().then(function(collection) {
+           then(collection);
+        }).catch(function(err) {
+           fail(err);
+       });
+ }	
+
 });
