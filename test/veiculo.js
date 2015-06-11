38b9c708c8d3a289694c75d41d42fb36d1ab530f
jeffersonarar <jeffersonarar@hotmail.com>
2015-06-11 12:45:51 -0300

    Testes feitos de procurar, gravar e procurarVeiculoPorPlaca

--- test/veiculo.js
+++ test/veiculo.js
@@ -2,73 +2,117 @@
 
 var AreaAzul = require('../areaazul');
 var should = require('chai').should();
-var Veiculo = AreaAzul.models.veiculo;
+var Veiculo = AreaAzul.models.Veiculo;
+var Veiculos = AreaAzul.collections.Veiculos;
 var Usuario = AreaAzul.models.Usuario;
 var Estado = AreaAzul.models.Estado;
+var TestHelpers = require('../helpers/test');
 
 describe('model.veiculo', function() {
-  var usuarioTeste = 'usuarioVeiculo';
-  var estadoTeste = 'estadoTeste';
+
+  function apagarDadosDeTeste() {
+    return TestHelpers.apagarVeiculoPorId(idVeiculo, idUsuarioComum);
+  }
+
+  var placaTeste = 'AAA1234';
+  var marcaTeste = 'Marca teste';
+  var modeloTeste = 'Modelo Teste';
+  var corTeste = 'Cor Teste';
+  var anoFabricadoTeste = '2015';
+  var anoModeloTeste = '2015';
+  var estadoTesteNome = 'Estado';
+  var estadoTesteUf = 'UF';
+  var idEstado = null;
+  var idVeiculo = null;
+  var idUsuarioComum = null;
+  var loginUsuarioComPreExistente = 'usuario-pre-existente';
+  var cpfUsuarioComumPreExistente = 'usuario-comum-test';
+
 
   before(function(done) {
-    done();
+    apagarDadosDeTeste()
+      .then(function() {
+        return Estado
+          .forge({nome: estadoTesteNome})
+          .fetch()
+          .then(function(e) {
+            if (e) { return e; }
+            return Estado.cadastrar({
+                nome: estadoTesteNome,
+                uf: estadoTesteUf,
+              });
+          });
+      })
+      .then(function(estado) {
+        idEstado = estado.id;
+      })
+      .then(function() {
+        done();
+      })
+      .catch(function(e) {
+        done(e);
+      });
   });
 
-  after(function(done) {
-    done();
-  });
   describe('cadastrar()', function() {
-    it.skip('grava veiculo', function(done) {
-      var veiculo = {
-        estado_id: 1,
-        placa: 'bbc',
-        placa_numero: 1234,
-        marca: 'Chrsyler',
-        modelo: '300c',
-        cor: 'preto',
-        ano_fabricado: 2015,
-        ano_modelo: 2015,
-        usuario_id: 31,
-      };
-      Veiculo.cadastrar(veiculo,
-        function(veiculo) {
-          should.exist(veiculo);
+    it('grava veiculo', function(done) {
+
+      Veiculo
+      .cadastrar({
+        estado_id: idEstado,
+        placa: placaTeste,
+        marca: marcaTeste,
+        modelo: modeloTeste,
+        cor: corTeste,
+        ano_fabricado: anoFabricadoTeste,
+        ano_modelo: anoModeloTeste,
+      })
+      .then(function(veiculo) {
+        idVeiculo = veiculo.id;
+        return done();
+      })
+      .catch(function(e) {
+        done(e);
+      });
+    });
+
+    describe('Procurar()', function() {
+    it('retorna um veiculo', function(done) {
+      var v = { id_veiculo: idVeiculo };
+      Veiculos.procurar(v,
+        function(model) {
+          should.exist(model);
           done();
         },
         function(err) {
           done(err);
         });
+    });
+  });
+    describe('procurarVeiculoPorPlaca()', function() {
+    it('retorna um veiculo', function(done) {
+      var v = { placa: placaTeste};
+      Veiculo.procurarVeiculoPorPlaca(v,
+          function(model) {
+            should.exist(model);  
+            done();
+          }, 
+          function(err) {
+            done(err);
+          });
     });
   });
 
-  describe('Procurar()', function() {
-    it.skip('retorna um veiculo', function(done) {
-      var v = { id_veiculo: 11 };
-      Veiculo.procurar(v,
-        function(model) {
-          should.exist(model);
-          model.should.not.be.empty();
-          done();
-        },
-        function(err) {
-          done(err);
-        });
-    });
+    after(function(done) {
+    apagarDadosDeTeste()
+      .then(function() {
+        done();
+      })
+      .catch(function(e) {
+        done(e);
+      });
   });
-  describe('procurarVeiculoPorPlaca()', function() {
-    it.skip('retorna um veiculo', function(done) {
-      var v = { placa: 'bbe1244'};
-      Veiculo.procurarVeiculoPorPlaca(v,
-        function(model) {
-          should.exist(model);
-          model.should.not.empty();
-          done();
-        },
-        function(err) {
-          console.log(err);
-          done(err);
-        });
-    });
+
   });
 
 });
