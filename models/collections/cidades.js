2c7d3b806b81e9613bc1183e21e6cc39b6a5e280
jeffersonarar <jeffersonarar@hotmail.com>
2015-09-02 11:23:33 -0300

    Criado método de listar cidades.

--- models/collections/cidades.js
+++ models/collections/cidades.js
@@ -3,6 +3,18 @@
 var Bookshelf = require('bookshelf').conexaoMain;
 var Cidade = require("../models/cidade");
 
-module.exports = Bookshelf.Collection.extend({
-  model: Cidade
+var CidadeCollection = Bookshelf.Collection.extend({
+    model: Cidade,
+}, {
+    listar: function(id_estado, func) {
+        CidadeCollection.forge().query(function(qb) {
+            qb.where('estado_id', '=', id_estado);
+            qb.select('cidade.*')
+        }).fetch().then(function(collection) {
+            console.log(collection.models);
+            func(collection);
+        });
+    },
 });
+
+module.exports = CidadeCollection;
