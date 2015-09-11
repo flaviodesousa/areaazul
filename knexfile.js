df7943dc49158cc94d192cd1e5413ca93271b54e
Flavio de Sousa <fl@viodesousa.com>
2015-09-11 19:18:54 +0200

    Seed de estados e cidades

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,9 @@ module.exports = {
     migrations: {
       tableName: 'migrations',
     },
+    seeds: {
+      directory: './seeds/dev'
+    },
   },
 
   staging: {
--- models/collections/estados.js
+++ models/collections/estados.js
@@ -5,19 +5,14 @@ var Estado = require("../models/estado");
 
 var EstadoCollection = Bookshelf.Collection.extend({
     model: Estado,
-
-
 }, {
-
     listar: function(func) {
         EstadoCollection.forge().query(function(qb) {
             qb.select('estado.*')
         }).fetch().then(function(collection) {
-            console.log(collection.models);
             func(collection);
         });
     },
-
-
 });
+
 module.exports = EstadoCollection;
