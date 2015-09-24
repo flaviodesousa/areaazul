c04b3f7302f65bed40c786dd8a7430c21ce0cd16
jeffersonarar <jeffersonarar@hotmail.com>
2015-09-24 11:08:22 -0300

    - Modificado atributos do array. - Removido importação das Collection,

--- models/models/configuracao.js
+++ models/models/configuracao.js
@@ -6,16 +6,28 @@ var util = require('./util');
 var Configuracao = Bookshelf.Model.extend({
     tableName: 'configuracao',
     idAttribute: 'id_configuracao'
-},{
+}, {
 
-  getConfiguracaoTempo: function(){
-    var tempos = [];
-    tempos.push({atribute: '60', valor: 2.00});
-    tempos.push({atribute: '120', valor: 4.00});
-    tempos.push({atribute: '180', valor: 6.00});
-    tempos.push({atribute: '180', valor: 8.00});
-    return tempos;
-  }
+    getConfiguracaoTempo: function() {
+        var tempos = [];
+        tempos.push({
+            quantidade_tempo: '60',
+            preco: 2.00
+        });
+        tempos.push({
+            quantidade_tempo: '120',
+            preco: 4.00
+        });
+        tempos.push({
+            quantidade_tempo: '180',
+            preco: 6.00
+        });
+        tempos.push({
+            quantidade_tempo: '180',
+            preco: 8.00
+        });
+        return tempos;
+    }
 });
 
 module.exports = Configuracao;
