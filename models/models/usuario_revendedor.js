7a10922a23e0d8aa2c75b2c393468323c553c744
jeffersonarar <jeffersonarar@hotmail.com>
2015-10-13 10:04:14 -0300

    - Criação do método procurar para buscar todos os dados para alteração de usuario revendedor.

--- models/models/usuario_revendedor.js
+++ models/models/usuario_revendedor.js
@@ -16,8 +16,8 @@ var UsuarioRevendedor = Bookshelf.Model.extend({
     tableName: 'usuario_revendedor',
     idAttribute: 'pessoa_fisica_pessoa_id',
     pessoaFisica: function() {
-    return this.hasOne(PessoaFisica, 'pessoa_id');
-}
+        return this.hasOne(PessoaFisica, 'pessoa_id');
+    }
 }, {
     autorizado: function(login, senha) {
         var UsuarioRevendedor = this;
@@ -67,59 +67,59 @@ var UsuarioRevendedor = Bookshelf.Model.extend({
                 var trxIns = _.merge({}, trx, {
                     method: 'insert'
                 });
-               
 
-            return UsuarioRevendedor
-                .validarUsuarioRevenda(user_reveller)
-                .then(function(messages) {
-                    if (messages.length) {
-                        throw new AreaAzul
-                            .BusinessException(
-                                'Nao foi possivel cadastrar nova Revenda. Dados invalidos',
-                                messages);
-                    }
-                    console.dir(messages);
-                    return messages;
-                })
-                .then(function() {
-                    return PessoaFisica
-                        .forge({
-                            cpf: user_reveller.cpf
-                        })
-                        .fetch()
-                        .then(function(pessoaFisica) {
-                            // Se pessoa fisica ja' existir, conectar a ela
-                            if (pessoaFisica !== null) {
-                                return pessoaFisica;
-                            }
-                            // Caso nao exista, criar a pessoa fisica
-                            return PessoaFisica
-                                ._cadastrar(user_reveller, trx);
-                        })
-                })
-                .then(function(pessoaFisica) {
-                    console.log("user_reveller" + user_reveller.login);
-                    return Usuario_Revendedor
-                        .forge({
-                            login: user_reveller.login,
-                            senha: senha,
-                            acesso_confirmado: false,
-                            ativo: true,
-                            autorizacao: user_reveller.autorizacao,
-                            revendedor_id: user_reveller.revendedor_id,
-                            pessoa_fisica_pessoa_id: pessoaFisica.get('pessoa_id'),
-                        })
-                        .save(null, trxIns);
-                })
-                .then(function(u_r) {
-                    usuario_revendedor = u_r;
-                    return u_r;
-                });
-        })
-        .then(function() {
-            return usuario_revendedor;
-        });
-},
+
+                return UsuarioRevendedor
+                    .validarUsuarioRevenda(user_reveller)
+                    .then(function(messages) {
+                        if (messages.length) {
+                            throw new AreaAzul
+                                .BusinessException(
+                                    'Nao foi possivel cadastrar nova Revenda. Dados invalidos',
+                                    messages);
+                        }
+                        console.dir(messages);
+                        return messages;
+                    })
+                    .then(function() {
+                        return PessoaFisica
+                            .forge({
+                                cpf: user_reveller.cpf
+                            })
+                            .fetch()
+                            .then(function(pessoaFisica) {
+                                // Se pessoa fisica ja' existir, conectar a ela
+                                if (pessoaFisica !== null) {
+                                    return pessoaFisica;
+                                }
+                                // Caso nao exista, criar a pessoa fisica
+                                return PessoaFisica
+                                    ._cadastrar(user_reveller, trx);
+                            })
+                    })
+                    .then(function(pessoaFisica) {
+                        console.log("user_reveller" + user_reveller.login);
+                        return Usuario_Revendedor
+                            .forge({
+                                login: user_reveller.login,
+                                senha: senha,
+                                acesso_confirmado: false,
+                                ativo: true,
+                                autorizacao: user_reveller.autorizacao,
+                                revendedor_id: user_reveller.revendedor_id,
+                                pessoa_fisica_pessoa_id: pessoaFisica.get('pessoa_id'),
+                            })
+                            .save(null, trxIns);
+                    })
+                    .then(function(u_r) {
+                        usuario_revendedor = u_r;
+                        return u_r;
+                    });
+            })
+            .then(function() {
+                return usuario_revendedor;
+            });
+    },
     search: function(entidade, func) {
         entidade.fetch().then(function(model, err) {
             if (model !== null)
@@ -162,203 +162,226 @@ var UsuarioRevendedor = Bookshelf.Model.extend({
         });
     },
 
-    validarSenha: function(user) {
-        var message = [];
-        if (user.nova_senha === null || user.nova_senha === '') {
-            message.push({
-                attribute: 'nova_senha',
-                problem: "Nova senha é obrigatório!"
-            });
-        }
-        if (user.senha === null || user.senha === '') {
-            message.push({
-                attribute: 'senha',
-                problem: "Senha é obrigatório!"
-            });
-        }
-        if (user.conf_senha === null || user.conf_senha === '') {
-            message.push({
-                attribute: 'conf_senha',
-                problem: "Confirmação de senha é obrigatório!"
-            });
-        }
-        if (user.nova_senha !== user.conf_senha) {
-            message.push({
-                attribute: 'nova_senha',
-                problem: "As senhas devem ser iguais!"
-            });
-        }
-        if (user.senha.length < 4 && user.senha.length > 8) {
-            message.push({
-                attribute: 'senha',
-                problem: "A senha deve conter no minimo 4 a 8 caracteres!"
-            });
-        }
-        if (user.conf_senha.length < 4 && user.conf_senha.length > 8) {
-            message.push({
-                attribute: 'conf_senha',
-                problem: "A confirmação de senha deve conter no minimo 4 a 8caracteres!"
-            });
-        }
-        if (user.nova_senha.length < 4 && user.nova_senha.length > 8) {
-            message.push({
-                attribute: 'nova_senha',
-                problem: "A nova senha deve conter no minimo 4 a 8 caracteres!"
-            });
-        }
-
-        for (var i = 0; i < message.length; i++) {
-            console.log("Atributo: " + message[i].attribute + " Problem: " + message[i].problem);
-        }
-
-        return message;
-    },
-    validarSenhaAlteracao: function(user) {
-        var message = [];
-
-        if (validator.isNull(user.conf_senha)) {
-            message.push({
-                attribute: 'nova_senha',
-                problem: 'Nova senha é obrigatória!',
-            });
-        }
-        if (validator.isNull(user.senha)) {
-            message.push({
-                attribute: 'senha',
-                problem: 'Senha é obrigatória!',
-            });
-        }
-
-        if (user.senha.length < 4) {
-            message.push({
-                attribute: 'senha',
-                problem: 'A senha deve conter no minimo 4 caracteres!',
-            });
-        }
-
-        if (user.conf_senha.length < 4) {
-            message.push({
-                attribute: 'nova_senha',
-                problem: 'A nova senha deve conter no minimo 4 caracteres!',
-            });
-        }
-        if (user.conf_senha !== user.senha) {
-            message.push({
-                attribute: 'nova_senha, senha',
-                problem: 'As senhas devem ser iguais!',
-            });
-        }
-        return message;
-    },
-    alterarSenhaRecuperacao: function(user) {
-        new this.Usuario_Revendedor({
-            pessoa_fisica_pessoa_id: user.pessoa_fisica_pessoa_id,
-        })
-            .fetch()
-            .then(function(model) {
-                if (model !== null) {
-                    var novaSenha = util.criptografa(user.senha);
-                    model.save({
-                        primeiro_acesso: 'false',
-                        senha: novaSenha,
-                        ativo: 'true',
-                    }).then(function(model) {
-                        then(model);
-                    }).catch(function(err) {
-                        fail(model.attributes);
-                    });
-                } else {
-                    throw new Error("Não encontrado!!!");
-                }
-            })
-            .catch(function(err) {
-                fail(err);
-            });
-    },
-    buscarPorId: function(id) {
-        return UsuarioRevendedor
-            .forge({
-                pessoa_fisica_pessoa_id: id
-            })
-            .fetch()
-            .then(function(u) {
-                if (u) {
-                    return u;
-                }
-                var err = new AreaAzul.BusinessException(
-                    'UsuarioAdministrativo: id nao encontrado', {
-                        id: id
-                    });
-                log.warn(err.message, err.details);
-                throw err;
+    procurar: function(id, func) {
+        UsuarioRevendedor.forge()
+            .query(function(qb) {
+                qb
+                    .distinct()
+                    .innerJoin('pessoa_fisica', function() {
+                        this.on('pessoa_fisica.pessoa_id', '=', 'usuario_revendedor.pessoa_fisica_pessoa_id');
+                    })
+                    .innerJoin('pessoa', function() {
+                        this.on('pessoa.id_pessoa', '=', 'pessoa_fisica.pessoa_id');
+                    })
+                    .where('usuario_revendedor.pessoa_fisica_pessoa_id', id)
+                    .select('pessoa_fisica.*')
+                    .select('pessoa.*')
+                    .select('usuario_revendedor.*');
+                    console.log('sql' + qb);
+            }).fetch().then(function(model) {
+                util.log(model);
+                func(model);
             });
     },
 
 
- validarUsuarioRevenda: function(user_reveller) {
-        var message = [];
-
-        if (!user_reveller.nome) {
-            message.push({
-                attribute: 'nome',
-                problem: 'Nome obrigatório!',
-            });
-        }
-
-        if (!user_reveller.email) {
-            message.push({
-                attribute: 'email',
-                problem: 'Email obrigatório!',
-            });
-        }
-
-        if (!user_reveller.login) {
-            message.push({
-                attribute: 'login',
-                problem: 'Login obrigatório!',
-            });
-        }
-
-        if (!validator.isEmail(user_reveller.email)) {
-            message.push({
-                attribute: 'email',
-                problem: 'Email inválido!',
-            });
-        }
-
-        if (!user_reveller.cpf) {
-            message.push({
-                attribute: 'cpf',
-                problem: 'CPF é obrigatório!',
-            });
-
-        }
-
-        if (!validation.isCPF(user_reveller.cpf)) {
-            message.push({
-                attribute: 'cpf',
-                problem: 'CPF inválido!',
-            });
-        }
-
-        return UsuarioRevendedor
-            .procurarLogin(user_reveller.login)
-            .then(function(usuariorevendedor) {
-                if (usuariorevendedor) {
-                    message.push({
-                        attribute: 'login',
-                        problem: 'Login já cadastrado!',
-                    });
-                }
-
-                return message;
+validarSenha: function(user) {
+    var message = [];
+    if (user.nova_senha === null || user.nova_senha === '') {
+        message.push({
+            attribute: 'nova_senha',
+            problem: "Nova senha é obrigatório!"
         });
-    },
-    procurarLogin: function(login) {
-        return this.forge({
-            login: login
-        }).fetch();
-    },
+    }
+    if (user.senha === null || user.senha === '') {
+        message.push({
+            attribute: 'senha',
+            problem: "Senha é obrigatório!"
+        });
+    }
+    if (user.conf_senha === null || user.conf_senha === '') {
+        message.push({
+            attribute: 'conf_senha',
+            problem: "Confirmação de senha é obrigatório!"
+        });
+    }
+    if (user.nova_senha !== user.conf_senha) {
+        message.push({
+            attribute: 'nova_senha',
+            problem: "As senhas devem ser iguais!"
+        });
+    }
+    if (user.senha.length < 4 && user.senha.length > 8) {
+        message.push({
+            attribute: 'senha',
+            problem: "A senha deve conter no minimo 4 a 8 caracteres!"
+        });
+    }
+    if (user.conf_senha.length < 4 && user.conf_senha.length > 8) {
+        message.push({
+            attribute: 'conf_senha',
+            problem: "A confirmação de senha deve conter no minimo 4 a 8caracteres!"
+        });
+    }
+    if (user.nova_senha.length < 4 && user.nova_senha.length > 8) {
+        message.push({
+            attribute: 'nova_senha',
+            problem: "A nova senha deve conter no minimo 4 a 8 caracteres!"
+        });
+    }
+
+    for (var i = 0; i < message.length; i++) {
+        console.log("Atributo: " + message[i].attribute + " Problem: " + message[i].problem);
+    }
+
+    return message;
+},
+validarSenhaAlteracao: function(user) {
+    var message = [];
+
+    if (validator.isNull(user.conf_senha)) {
+        message.push({
+            attribute: 'nova_senha',
+            problem: 'Nova senha é obrigatória!',
+        });
+    }
+    if (validator.isNull(user.senha)) {
+        message.push({
+            attribute: 'senha',
+            problem: 'Senha é obrigatória!',
+        });
+    }
+
+    if (user.senha.length < 4) {
+        message.push({
+            attribute: 'senha',
+            problem: 'A senha deve conter no minimo 4 caracteres!',
+        });
+    }
+
+    if (user.conf_senha.length < 4) {
+        message.push({
+            attribute: 'nova_senha',
+            problem: 'A nova senha deve conter no minimo 4 caracteres!',
+        });
+    }
+    if (user.conf_senha !== user.senha) {
+        message.push({
+            attribute: 'nova_senha, senha',
+            problem: 'As senhas devem ser iguais!',
+        });
+    }
+    return message;
+},
+alterarSenhaRecuperacao: function(user) {
+    new this.Usuario_Revendedor({
+        pessoa_fisica_pessoa_id: user.pessoa_fisica_pessoa_id,
+    })
+        .fetch()
+        .then(function(model) {
+            if (model !== null) {
+                var novaSenha = util.criptografa(user.senha);
+                model.save({
+                    primeiro_acesso: 'false',
+                    senha: novaSenha,
+                    ativo: 'true',
+                }).then(function(model) {
+                    then(model);
+                }).catch(function(err) {
+                    fail(model.attributes);
+                });
+            } else {
+                throw new Error("Não encontrado!!!");
+            }
+        })
+        .catch(function(err) {
+            fail(err);
+        });
+},
+buscarPorId: function(id) {
+    return UsuarioRevendedor
+        .forge({
+            pessoa_fisica_pessoa_id: id
+        })
+        .fetch()
+        .then(function(u) {
+            if (u) {
+                return u;
+            }
+            var err = new AreaAzul.BusinessException(
+                'UsuarioAdministrativo: id nao encontrado', {
+                    id: id
+                });
+            log.warn(err.message, err.details);
+            throw err;
+        });
+},
+
+
+validarUsuarioRevenda: function(user_reveller) {
+    var message = [];
+
+    if (!user_reveller.nome) {
+        message.push({
+            attribute: 'nome',
+            problem: 'Nome obrigatório!',
+        });
+    }
+
+    if (!user_reveller.email) {
+        message.push({
+            attribute: 'email',
+            problem: 'Email obrigatório!',
+        });
+    }
+
+    if (!user_reveller.login) {
+        message.push({
+            attribute: 'login',
+            problem: 'Login obrigatório!',
+        });
+    }
+
+    if (!validator.isEmail(user_reveller.email)) {
+        message.push({
+            attribute: 'email',
+            problem: 'Email inválido!',
+        });
+    }
+
+    if (!user_reveller.cpf) {
+        message.push({
+            attribute: 'cpf',
+            problem: 'CPF é obrigatório!',
+        });
+
+    }
+
+    if (!validation.isCPF(user_reveller.cpf)) {
+        message.push({
+            attribute: 'cpf',
+            problem: 'CPF inválido!',
+        });
+    }
+
+    return UsuarioRevendedor
+        .procurarLogin(user_reveller.login)
+        .then(function(usuariorevendedor) {
+            if (usuariorevendedor) {
+                message.push({
+                    attribute: 'login',
+                    problem: 'Login já cadastrado!',
+                });
+            }
+
+            return message;
+        });
+},
+procurarLogin: function(login) {
+    return this.forge({
+        login: login
+    }).fetch();
+},
 
 
 });
