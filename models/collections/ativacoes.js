d61993cf490986c9dbf71fd7673ade9c8b3d9a2f
jeffersonarar <jeffersonarar@hotmail.com>
2015-11-05 13:29:44 -0200

    - Implementação de métodos para buscar veículos distintos e separa-los em um array de hashmap; - Implementação de métodos de buscar com base em necessidades de segregação;

--- models/collections/ativacoes.js
+++ models/collections/ativacoes.js
@@ -2,36 +2,40 @@ var Bookshelf = require('bookshelf').conexaoMain;
 var Ativacao = require('../models/ativacao');
 var _ = require('lodash');
 
-module.exports = Bookshelf.Collection.extend({
-  model: Ativacao,
+var AtivacaoCollection = Bookshelf.Collection.extend({
+    model: Ativacao,
 },{
 
-	_listarAtivacoes: function() {
-    return this
-      .query(function(qb) {
-      qb
-        .innerJoin('veiculo', function() {
-          this.on('veiculo.id_veiculo', '=','ativacao.veiculo_id');
-        })
-        .leftJoin('fiscalizacao', function() {
-          this.on('fiscalizacao.veiculo_id', '!=', 'ativacao.veiculo_id');
-        })
-
-        .select('ativacao.*')
-        .select('veiculo.*')
-        .select('fiscalizacao.*');
-      console.log('sql' + qb);
-    })
-      .fetch()
-      .then(function(listaAtivacoes) {
-      	console.log(listaAtivacoes);
-        return listaAtivacoes;
-      })
-      .catch(function(err) {
-        return err;
-      });
-  },
+  _listarAtivacoes: function(func) {
 
 
+        return AtivacaoCollection.forge().query(function(qb) {
+                qb
+                    .innerJoin('veiculo', function() {
+                        this.on('veiculo.id_veiculo', '=', 'ativacao.veiculo_id');
+                    })
+                    .leftJoin('fiscalizacao', function() {
+                        this.on('fiscalizacao.veiculo_id', '!=', 'ativacao.veiculo_id');
+                    })
+
+                    .select('ativacao.*')
+                    .select('veiculo.*');
+                console.log('sql' + qb);
+            }).fetch().then(function(collectionVeiculosSomenteAtivados) {
+
+                return collectionVeiculosSomenteAtivados;
+            });
+    },
+
+    _listaTodos: function(func) {
+        AtivacaoCollection.forge().query(function(qb) {
+            qb.select('ativacao.*')
+        }).fetch().then(function(collection) {
+            return collection;
+        });
+    },
 
 });
+
+
+module.exports = AtivacaoCollection;
--- models/collections/veiculo.js
+++ models/collections/veiculo.js
@@ -2,47 +2,66 @@
 
 var Bookshelf = require('bookshelf').conexaoMain;
 var Veiculo = require('../models/veiculo');
-
+var Ativacoes = require('../collections/ativacoes');
+var Fiscalizacoes = require('../collections/fiscalizacoes');
+var Veiculos = require('../collections/veiculo');
+var _ = require('lodash');
 
 var VeiculoCollection = Bookshelf.Collection.extend({
     model: Veiculo,
 }, {
 
     procurar: function(vehicle, then, fail) {
-        Veiculo
-            .forge()
-            .query(function(qb) {
-                qb.where('veiculo.id_veiculo', vehicle.id_veiculo);
-                qb.join('cidade', 'cidade.id_cidade', '=', 'veiculo.cidade_id');
-                qb.select('veiculo.*');
-                qb.select('cidade.*');
-            })
-            .fetch()
-            .then(function(collection) {
-                then(collection);
-            })
-            .catch(function(err) {
-                fail(err);
-            });
+        Veiculo.forge().query(function(qb) {
+            qb.where('veiculo.id_veiculo', vehicle.id_veiculo);
+            qb.join('cidade', 'cidade.id_cidade', '=', 'veiculo.cidade_id');
+            qb.select('veiculo.*');
+            qb.select('cidade.*');
+        }).fetch().then(function(collection) {
+            then(collection);
+        }).catch(function(err) {
+            fail(err);
+        });
     },
 
     listar: function(func) {
-        VeiculoCollection.forge().query(function(qb) {
-            qb
-            .innerJoin('veiculo', function() {
-              this.on('veiculo.id_veiculo', '=','ativacao.veiculo_id');
-            })
-            .leftJoin('fiscalizacao', function() {
-              this.on('fiscalizacao.veiculo_id', '!=', 'ativacao.veiculo_id');
-            })
+        var arrayCampos = new Array();
+        var i = null;
+        var person = new Object();
+        return Ativacoes._listarAtivacoes().
+        then(function(collectionVeiculosSomenteAtivados) {
 
-            .select('ativacao.*')
-            .select('veiculo.*')
-            .select('fiscalizacao.*');
-            console.log('sql' + qb);
+            arrayCampos["ativo"] = collectionVeiculosSomenteAtivados;
+            return arrayCampos;
+
+        }).then(function(arrayCampos) {
+
+            return Fiscalizacoes._listarFiscalizacoes()
+                .then(function(collectionVeiculosSomenteFiscalizados) {
+
+                    arrayCampos["fiscalizado"] = collectionVeiculosSomenteFiscalizados;
+                    return arrayCampos;
+
+                })
+
+        }).then(function(arrayCampos) {
+
+            return Fiscalizacoes._listarFiscalizacoesToleradas()
+                .then(function(collectionVeiculosTolerancia) {
+
+                    arrayCampos["tolerancia"] = collectionVeiculosTolerancia;
+                    func(arrayCampos);
+                    console.log("-------------------------------------");
+                    console.dir(arrayCampos);
+                     })
+                });
+    },
+
+    _listaTodos: function(func) {
+        VeiculoCollection.forge().query(function(qb) {
+            qb.select('veiculo.*')
         }).fetch().then(function(collection) {
-          console.dir(collection);
-            func(collection);
+            return collection;
         });
     },
 
