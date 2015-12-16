f3c63b6dca1135778ecb9afc90f11b6c6415b994
Willian Brener <willian_babao@hotmail.com>
2015-12-16 12:06:57 -0200

    - Implementação de método que realiza pesquisa de movimentação de usuário;

--- models/collections/movimentacaoconta.js
+++ models/collections/movimentacaoconta.js
@@ -1,6 +1,32 @@
+'use strict';
+
 var Bookshelf = require('bookshelf').conexaoMain;
 var MovimentacaoConta = require("../models/movimentacaoconta");
+var moment = require('moment');
 
-module.exports = Bookshelf.Collection.extend({
-    model: MovimentacaoConta
+
+var MovimentacaoContaCollection = Bookshelf.Collection.extend({
+    model: MovimentacaoConta,
+}, {
+
+     listarMovimentacaoUsuario: function(id) {
+
+        return  this
+                .forge()
+                .query(function(qb) {
+                    qb
+                .innerJoin('usuario', function() {
+                    this.on('usuario.pessoa_id', '=', 'movimentacao_conta.pessoa_id');
+                })
+                .where('usuario.pessoa_id', '=', id)
+                .select('movimentacao_conta.*');
+            console.log('sql' + qb);
+            }).fetch()
+                .then(function(collection) {
+            return collection;
+        });
+    },
+
 });
+
+module.exports = MovimentacaoContaCollection;
