df8e6544ee458b5066f939bf6759a306d6669a86
Flavio de Sousa <fl@viodesousa.com>
2015-12-17 22:02:33 +0100

    Suporte a migrate:latest automatico removido

--- configuration/database.js
+++ configuration/database.js
@@ -5,9 +5,5 @@ var env = process.env.NODE_ENV || 'development';
 
 var knex = require('knex')(knexfile[env]);
 
-knex.migrate.latest({
-  directory: path.join(__dirname, '..', 'migrations')
-});
-
 exports.Bookshelf = require('bookshelf');
 exports.Bookshelf.conexaoMain = require('bookshelf')(knex);
