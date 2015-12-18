7de27a4f59c48776cdab048518d10948d4ce485b
Willian Brener <willian_babao@hotmail.com>
2015-12-18 12:16:35 -0200

    - Implementação de método para conversão de data para string;

--- helpers/util.js
+++ helpers/util.js
@@ -91,3 +91,18 @@ exports.placaSemMascara = function(valor) {
 
   return valorComMascara;
 };
+
+exports.stringToDate = function(_date,_format,_delimiter){
+  
+  var formatLowerCase=_format.toLowerCase();
+  var formatItems=formatLowerCase.split(_delimiter);
+  var dateItems=_date.split(_delimiter);
+  var monthIndex=formatItems.indexOf("mm");
+  var dayIndex=formatItems.indexOf("dd");
+  var yearIndex=formatItems.indexOf("yyyy");
+  var month=parseInt(dateItems[monthIndex]);
+  month-=1;
+  var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
+
+  return formatedDate;
+};
