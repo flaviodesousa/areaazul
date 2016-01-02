71089cfe328c0ed44ccc99cd67a776c2311eee5b
Flavio de Sousa <fl@viodesousa.com>
2016-01-02 22:46:20 +0100

    Linter recommendations

--- helpers/util.js
+++ helpers/util.js
@@ -4,20 +4,19 @@ var AreaAzul = require('../../areaazul');
 var bcrypt = require('bcrypt');
 var AreaAzulMailer = require('areaazul-mailer');
 var moment = require('moment');
-var winston = require('winston');
 var uuid = require('node-uuid');
 
 exports.enviarEmailConfirmacao = function(entidade, login, senha) {
   var message = {
     from: 'AreaAzul <cadastro@areaazul.org>',
-    to:  entidade.email,
+    to: entidade.email,
     cc: 'cadastro@areaazul.org',
     subject: 'Confirmação de cadastro - AreaAzul',
     html: '<p>Por favor ' + entidade.nome +
-        ' clique no link abaixo para acessar a aplicação areaazul.</br>' +
-        'http://usuario.demo.areaazul.org/</br>' +
-        'Usuario: ' + login + '</br>' +
-        'Senha é: ' + senha + '.',
+      ' clique no link abaixo para acessar a aplicação areaazul.</br>' +
+      'http://usuario.demo.areaazul.org/</br>' +
+      'Usuario: ' + login + '</br>' +
+      'Senha é: ' + senha + '.',
   };
   AreaAzulMailer.enviar.emailer(message);
 };
@@ -25,22 +24,26 @@ exports.enviarEmailConfirmacao = function(entidade, login, senha) {
 exports.enviarEmailNovaSenha = function(email, nome, uuid) {
   var message = {
     from: 'AreaAzul <cadastro@areaazul.org>',
-    to:  email,
+    to: email,
     cc: 'cadastro@areaazul.org',
     subject: 'AreaAzul nova senha ',
     html: '<p>Por favor ' + nome +
-        ' clique no link abaixo para alterar sua senha.</br>' +
-        'http://demo.areaazul.org/' + uuid + '.',
+      ' clique no link abaixo para alterar sua senha.</br>' +
+      'http://demo.areaazul.org/' + uuid + '.',
   };
   AreaAzulMailer.enviar.emailer(message);
 };
 
 function getRandomChar() {
-  var ascii = [[48, 57], [64, 90], [97, 122]];
+  var ascii = [
+    [48, 57],
+    [64, 90],
+    [97, 122],
+  ];
   var i = Math.floor(Math.random() * ascii.length);
   return String.fromCharCode(
     Math.floor(Math.random() *
-     (ascii[i][1] - ascii[i][0])) + ascii[i][0]);
+      (ascii[i][1] - ascii[i][0])) + ascii[i][0]);
 }
 
 exports.generate = function() {
@@ -92,45 +95,47 @@ exports.placaSemMascara = function(valor) {
   return valorComMascara;
 };
 
-exports.stringToDate = function(_date,_format,_delimiter){
-  
-  var formatLowerCase=_format.toLowerCase();
-  var formatItems=formatLowerCase.split(_delimiter);
-  var dateItems=_date.split(_delimiter);
-  var monthIndex=formatItems.indexOf("mm");
-  var dayIndex=formatItems.indexOf("dd");
-  var yearIndex=formatItems.indexOf("yyyy");
-  var month=parseInt(dateItems[monthIndex]);
-  month-=1;
-  var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
+exports.stringToDate = function(_date, _format, _delimiter) {
+
+  var formatLowerCase = _format.toLowerCase();
+  var formatItems = formatLowerCase.split(_delimiter);
+  var dateItems = _date.split(_delimiter);
+  var monthIndex = formatItems.indexOf('mm');
+  var dayIndex = formatItems.indexOf('dd');
+  var yearIndex = formatItems.indexOf('yyyy');
+  var month = parseInt(dateItems[monthIndex]);
+  month -= 1;
+  var formatedDate = new Date(dateItems[yearIndex], month, dateItems[dayIndex]);
 
   return formatedDate;
 };
 
-exports.formataData = function(_data){
-  var dia = _data.substring(0,2); 
-  var mes = _data.substring(3,5); 
-  var ano = _data.substring(6,10);
-  var barra = "/";
-  var data = ano.concat(barra,mes,barra,dia); 
+exports.formataData = function(_data) {
+  var dia = _data.substring(0, 2);
+  var mes = _data.substring(3, 5);
+  var ano = _data.substring(6, 10);
+  var barra = '/';
+  var data = ano.concat(barra, mes, barra, dia);
   return data;
 };
 
-exports.dataValida = function(_data){
-  var dia = _data.substring(0,2); 
-  var mes = _data.substring(3,5); 
-  var ano = _data.substring(6,10);
-  var situacao = "valida";
-    if ((dia < 1)||(dia < 1 || dia > 30) && (  mes === 4 || mes === 6 || mes === 09 || mes === 11 ) || dia > 31) { 
-        situacao = "invalida"; 
-    }
-      if (mes < 1 || mes > 12 ) { 
-        situacao = "invalida"; 
-    } 
+exports.dataValida = function(_data) {
+  var dia = _data.substring(0, 2);
+  var mes = _data.substring(3, 5);
+  var ano = _data.substring(6, 10);
+  var situacao = 'valida';
+  if ((dia < 1) || (dia < 1 || dia > 30) && (mes === 4 || mes === 6 || mes ===
+      09 || mes === 11) || dia > 31) {
+    situacao = 'invalida';
+  }
+  if (mes < 1 || mes > 12) {
+    situacao = 'invalida';
+  }
 
-    // verifica se e ano bissexto 
-    if (mes === 2 && ( dia < 1 || dia > 29 || ( dia > 28 && (parseInt(ano / 4) !== ano / 4)))) { 
-        situacao = "invalida"; 
-    }  
-    return situacao;
+  // Verifica se e ano bissexto
+  if (mes === 2 && (dia < 1 || dia > 29 || (dia > 28 && (parseInt(ano / 4) !==
+      ano / 4)))) {
+    situacao = 'invalida';
+  }
+  return situacao;
 };
--- models/models/ativacao.js
+++ models/models/ativacao.js
@@ -15,296 +15,299 @@ var UsuarioRevendedor = require('./usuario_revendedor');
 
 
 var Ativacao = Bookshelf.Model.extend({
-    tableName: 'ativacao',
-    idAttribute: 'id_ativacao',
+  tableName: 'ativacao',
+  idAttribute: 'id_ativacao',
 
 }, {
 
-    ativar: function(activation) {
+  ativar: function(activation) {
 
-        var latitude = activation.latitude;
-        var altitude = activation.longitude;
-        var longitude = activation.altitude;
+    var latitude = activation.latitude;
+    var altitude = activation.longitude;
+    var longitude = activation.altitude;
 
-        if (validator.isNull(latitude)) {
-            latitude = null;
-        }
-        if (validator.isNull(longitude)) {
-            longitude = null;
-        }
-        if (validator.isNull(altitude)) {
-            altitude = null;
-        }
+    if (validator.isNull(latitude)) {
+      latitude = null;
+    }
+    if (validator.isNull(longitude)) {
+      longitude = null;
+    }
+    if (validator.isNull(altitude)) {
+      altitude = null;
+    }
 
-        return Bookshelf.transaction(function(t) {
-            var options = {
-                transacting: t,
-            };
-            var optionsInsert = {
-                transacting: t,
-                method: 'insert',
-            };
-            var optionsUpdate = {
-                transacting: t,
-                method: 'update',
-                patch: true,
-            };
-            var ativacao;
+    return Bookshelf.transaction(function(t) {
+      var options = {
+        transacting: t,
+      };
+      var optionsInsert = {
+        transacting: t,
+        method: 'insert',
+      };
+      var optionsUpdate = {
+        transacting: t,
+        method: 'update',
+        patch: true,
+      };
+      var ativacao;
 
-            return Ativacao
-                .forge({
-                    data_ativacao: new Date(),
-                    latitude: latitude,
-                    longitude: longitude,
-                    altitude: altitude,
-                    pessoa_id: activation.usuario_pessoa_id,
-                    veiculo_id: activation.veiculo_id,
-                    ativo: true,
-                })
-                .save(null, optionsInsert)
-                .then(function(a) {
-                    ativacao = a;
-                    return UsuarioHasVeiculo
-                        .forge({
-                            usuario_pessoa_id: activation.usuario_pessoa_id,
-                            veiculo_id: activation.veiculo_id,
-                        })
-                        .fetch()
-                        .then(function(usuariohasveiculo) {
-                            if (usuariohasveiculo === null) {
-                                return UsuarioHasVeiculo
-                                    .forge({
-                                        usuario_pessoa_id: activation.usuario_pessoa_id,
-                                        veiculo_id: activation.veiculo_id,
-                                        ultima_ativacao: new Date(),
-                                    })
-                                    .save(null, optionsInsert);
-                            } else {
-                                return usuariohasveiculo
-                                    .save({
-                                        ultima_ativacao: new Date(),
-                                    }, optionsUpdate);
-                            }
-                        })
-                        .then(function() {
-                            return MovimentacaoConta
-                                ._inserirDebito({
-                                    historico: 'ativacao',
-                                    tipo: 'ativacao',
-                                    pessoa_id: activation.usuario_pessoa_id,
-                                    valor: activation.valor,
-                                }, options);
-                        });
-                })
-                .then(function() {
-                    return ativacao;
-                });
-        });
-    },
-
-    desativar: function(desativacao) {
-        return Ativacao
+      return Ativacao
+        .forge({
+          data_ativacao: new Date(),
+          latitude: latitude,
+          longitude: longitude,
+          altitude: altitude,
+          pessoa_id: activation.usuario_pessoa_id,
+          veiculo_id: activation.veiculo_id,
+          ativo: true,
+        })
+        .save(null, optionsInsert)
+        .then(function(a) {
+          ativacao = a;
+          return UsuarioHasVeiculo
             .forge({
-                id_ativacao: desativacao.id_ativacao,
-                pessoa_id: desativacao.usuario_pessoa_id,
+              usuario_pessoa_id: activation.usuario_pessoa_id,
+              veiculo_id: activation.veiculo_id,
             })
             .fetch()
-            .then(function(d) {
-                if (!d) {
-                    var err = new AreaAzul.BusinessException(
-                        'Desativacao: Ativacao nao reconhecida', {
-                            desativacao: desativacao,
-                        });
-                    log.error(err.message, err.details);
-                    throw err;
-                }
-                return d;
-            })
-            .then(function(d) {
-                return d
-                    .save({
-                        data_desativacao: new Date(),
-                    }, {
-                        patch: true,
-                    });
-            })
-            .then(function(ativacaoExistente) {
-                log.info('Desativacao: sucesso', {
-                    desativacao: ativacaoExistente,
-                });
-                return ativacaoExistente;
-            });
-    },
-
-    ativarPelaRevenda: function(ativacao) {
-
-        return Bookshelf.transaction(function(t) {
-            var options = {
-                transacting: t,
-            };
-            var optionsInsert = {
-                transacting: t,
-                method: 'insert',
-            };
-            var idVeiculo = null;
-            var placaSemMascara = '';
-            if (ativacao.placa) {
-                placaSemMascara = util.placaSemMascara(ativacao.placa);
-            }
-            return Ativacao
-                .validarAtivacao(ativacao,placaSemMascara)
-                .then(function(messages) {
-                    if (messages.length) {
-                        throw new AreaAzul
-                            .BusinessException(
-                                'Nao foi possivel ativar veículo. Dados invalidos',
-                                messages);
-                    }
-
-                    return messages;
-                }).then(function() {
-                    return Veiculo
-                        .forge({
-                            placa: placaSemMascara,
-                        }).fetch();
-                }).then(function(veiculo) {
-                    if (veiculo) {
-                        return veiculo;
-                    } else {
-                        return Veiculo.cadastrar({
-                            placa: placaSemMascara,
-                            marca: ativacao.marca,
-                            cor: ativacao.cor,
-                            modelo: ativacao.modelo,
-                            cidade_id: ativacao.cidade,
-                        }, options);
-                    }
-                })
-                .then(function(v) {
-                    return Ativacao
-                        .forge({
-                            data_ativacao: new Date(),
-                            pessoa_id: ativacao.usuario_pessoa_id,
-                            veiculo_id: v.id,
-                            ativo: true,
-                        })
-                        .save(null, optionsInsert)
-                        .then(function(a) {
-                            return UsuarioRevendedor
-                                   .forge({pessoa_fisica_pessoa_id: a.get('pessoa_id')})
-                                   .fetch();
-                        })
-                        .then(function(usuario){
-                            return MovimentacaoConta
-                            ._inserirDebito({
-                                historico: 'ativacao',
-                                tipo: 'ativacao',
-                                pessoa_id: usuario.get('revendedor_id'),
-                                valor: 10.00,
-                            }, options);
-                        });
-                });
-        });
-    },
-
-    validarAtivacao: function(ativacao, placa) {
-
-        var message = [];
-
-
-
-        if (validator.isNull(ativacao.marca)) {
-            message.push({
-                attribute: 'marca',
-                problem: 'Campo marca é obrigatório!',
-            });
-        }
-        if (validator.isNull(ativacao.modelo)) {
-            message.push({
-                attribute: 'modelo',
-                problem: 'Campo modelo é obrigatório!',
-            });
-        }
-        if (validator.isNull(ativacao.cor)) {
-            message.push({
-                attribute: 'cor',
-                problem: 'Campo cor é obrigatório!',
-            });
-        }
-        if (validator.isNull(ativacao.tempo)) {
-            message.push({
-                attribute: 'tempo',
-                problem: 'Campo tempo é obrigatório!',
-            });
-        }
-        return Ativacao
-            .verificaAtivacao(placa)
-            .then(function(ativado) {
-                if (ativado) {
-                    message.push({
-                        attribute: 'ativado',
-                        problem: 'Este veículo está com uso ativado!',
-                    });
-                }
-                return message;
-
+            .then(function(usuariohasveiculo) {
+              if (usuariohasveiculo === null) {
+                return UsuarioHasVeiculo
+                  .forge({
+                    usuario_pessoa_id: activation.usuario_pessoa_id,
+                    veiculo_id: activation.veiculo_id,
+                    ultima_ativacao: new Date(),
+                  })
+                  .save(null, optionsInsert);
+              } else {
+                return usuariohasveiculo
+                  .save({
+                    ultima_ativacao: new Date(),
+                  }, optionsUpdate);
+              }
             })
             .then(function() {
-                return Ativacao
-                    .verificaSaldo(ativacao.usuario_pessoa_id)
-                    .then(function(conta) {
-                        if (!conta || conta.get('saldo') <= 0) {
-                            message.push({
-                                attribute: 'saldo',
-                                problem: 'Usuário não possui saldo em conta!',
-                            });
-                        }
-                        return message;
-                    });
+              return MovimentacaoConta
+                ._inserirDebito({
+                  historico: 'ativacao',
+                  tipo: 'ativacao',
+                  pessoa_id: activation.usuario_pessoa_id,
+                  valor: activation.valor,
+                }, options);
             });
+        })
+        .then(function() {
+          return ativacao;
+        });
+    });
+  },
 
+  desativar: function(desativacao) {
+    return Ativacao
+      .forge({
+        id_ativacao: desativacao.id_ativacao,
+        pessoa_id: desativacao.usuario_pessoa_id,
+      })
+      .fetch()
+      .then(function(d) {
+        if (!d) {
+          var err = new AreaAzul.BusinessException(
+            'Desativacao: Ativacao nao reconhecida', {
+              desativacao: desativacao,
+            });
+          log.error(err.message, err.details);
+          throw err;
+        }
+        return d;
+      })
+      .then(function(d) {
+        return d
+          .save({
+            data_desativacao: new Date(),
+          }, {
+            patch: true,
+          });
+      })
+      .then(function(ativacaoExistente) {
+        log.info('Desativacao: sucesso', {
+          desativacao: ativacaoExistente,
+        });
+        return ativacaoExistente;
+      });
+  },
 
-    },
+  ativarPelaRevenda: function(ativacao) {
 
+    return Bookshelf.transaction(function(t) {
+      var options = {
+        transacting: t,
+      };
+      var optionsInsert = {
+        transacting: t,
+        method: 'insert',
+      };
+      var placaSemMascara = '';
+      if (ativacao.placa) {
+        placaSemMascara = util.placaSemMascara(ativacao.placa);
+      }
+      return Ativacao
+        .validarAtivacao(ativacao, placaSemMascara)
+        .then(function(messages) {
+          if (messages.length) {
+            throw new AreaAzul
+              .BusinessException(
+                'Nao foi possivel ativar veículo. Dados invalidos',
+                messages);
+          }
 
-    verificaSaldo: function(id) {
-        return Conta
-            .forge()
-            .query(function(qb) {
-                qb
-                    .innerJoin('pessoa', function() {
-                        this.on('pessoa.id_pessoa', '=', 'conta.pessoa_id');
-                    })
-                    .innerJoin('revendedor', function() {
-                        this.on('revendedor.pessoa_id', '=', 'pessoa.id_pessoa');
-                    })
-                    .innerJoin('usuario_revendedor', function() {
-                        this.on('usuario_revendedor.revendedor_id', '=', 'revendedor.pessoa_id');
-                    })
-                    .where('usuario_revendedor.pessoa_fisica_pessoa_id', id)
-                    .select('pessoa.*')
-                    .select('conta.*')
-                    .select('revendedor.*')
-                    .select('usuario_revendedor.*');
+          return messages;
+        }).then(function() {
+          return Veiculo
+            .forge({
+              placa: placaSemMascara,
+            }).fetch();
+        }).then(function(veiculo) {
+          if (veiculo) {
+            return veiculo;
+          } else {
+            return Veiculo.cadastrar({
+              placa: placaSemMascara,
+              marca: ativacao.marca,
+              cor: ativacao.cor,
+              modelo: ativacao.modelo,
+              cidade_id: ativacao.cidade,
+            }, options);
+          }
+        })
+        .then(function(v) {
+          return Ativacao
+            .forge({
+              data_ativacao: new Date(),
+              pessoa_id: ativacao.usuario_pessoa_id,
+              veiculo_id: v.id,
+              ativo: true,
             })
-            .fetch();
-    },
+            .save(null, optionsInsert)
+            .then(function(a) {
+              return UsuarioRevendedor
+                .forge({
+                  pessoa_fisica_pessoa_id: a.get('pessoa_id')
+                })
+                .fetch();
+            })
+            .then(function(usuario) {
+              return MovimentacaoConta
+                ._inserirDebito({
+                  historico: 'ativacao',
+                  tipo: 'ativacao',
+                  pessoa_id: usuario.get('revendedor_id'),
+                  valor: 10.00,
+                }, options);
+            });
+        });
+    });
+  },
 
-    verificaAtivacao: function(placa) {
+  validarAtivacao: function(ativacao, placa) {
+
+    var message = [];
+
+
+
+    if (validator.isNull(ativacao.marca)) {
+      message.push({
+        attribute: 'marca',
+        problem: 'Campo marca é obrigatório!',
+      });
+    }
+    if (validator.isNull(ativacao.modelo)) {
+      message.push({
+        attribute: 'modelo',
+        problem: 'Campo modelo é obrigatório!',
+      });
+    }
+    if (validator.isNull(ativacao.cor)) {
+      message.push({
+        attribute: 'cor',
+        problem: 'Campo cor é obrigatório!',
+      });
+    }
+    if (validator.isNull(ativacao.tempo)) {
+      message.push({
+        attribute: 'tempo',
+        problem: 'Campo tempo é obrigatório!',
+      });
+    }
+    return Ativacao
+      .verificaAtivacao(placa)
+      .then(function(ativado) {
+        if (ativado) {
+          message.push({
+            attribute: 'ativado',
+            problem: 'Este veículo está com uso ativado!',
+          });
+        }
+        return message;
+
+      })
+      .then(function() {
         return Ativacao
-            .forge()
-            .query(function(qb) {
-                qb
-                    .innerJoin('veiculo', function() {
-                        this.on('veiculo.id_veiculo', '=', 'ativacao.veiculo_id');
-                    })
-                    .where('ativacao.data_ativacao', '>=', moment().subtract(60, 'minutes').calendar())
-                    .andWhere('veiculo.placa', '=', placa)
-                    .select('ativacao.*')
-                    .select('veiculo.*');
-            })
-            .fetch();
-    },
+          .verificaSaldo(ativacao.usuario_pessoa_id)
+          .then(function(conta) {
+            if (!conta || conta.get('saldo') <= 0) {
+              message.push({
+                attribute: 'saldo',
+                problem: 'Usuário não possui saldo em conta!',
+              });
+            }
+            return message;
+          });
+      });
+
+
+  },
+
+
+  verificaSaldo: function(id) {
+    return Conta
+      .forge()
+      .query(function(qb) {
+        qb
+          .innerJoin('pessoa', function() {
+            this.on('pessoa.id_pessoa', '=', 'conta.pessoa_id');
+          })
+          .innerJoin('revendedor', function() {
+            this.on('revendedor.pessoa_id', '=', 'pessoa.id_pessoa');
+          })
+          .innerJoin('usuario_revendedor', function() {
+            this.on('usuario_revendedor.revendedor_id', '=',
+              'revendedor.pessoa_id');
+          })
+          .where('usuario_revendedor.pessoa_fisica_pessoa_id', id)
+          .select('pessoa.*')
+          .select('conta.*')
+          .select('revendedor.*')
+          .select('usuario_revendedor.*');
+      })
+      .fetch();
+  },
+
+  verificaAtivacao: function(placa) {
+    return Ativacao
+      .forge()
+      .query(function(qb) {
+        qb
+          .innerJoin('veiculo', function() {
+            this.on('veiculo.id_veiculo', '=', 'ativacao.veiculo_id');
+          })
+          .where('ativacao.data_ativacao', '>=', moment().subtract(60,
+            'minutes').calendar())
+          .andWhere('veiculo.placa', '=', placa)
+          .select('ativacao.*')
+          .select('veiculo.*');
+      })
+      .fetch();
+  },
 
 });
 
