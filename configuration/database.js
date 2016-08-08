89774b35f618ba89f133eced6e9a20525728dd76
Flavio de Sousa <fl@viodesousa.com>
2016-08-08 21:28:22 +0200

    bookshelf registry plugin

--- configuration/database.js
+++ configuration/database.js
@@ -1,8 +1,14 @@
+var debug = require('debug')('areaazul:configuration:database');
+
 var knexfile = require('../knexfile');
 
 var env = process.env.NODE_ENV || 'development';
 
 var knex = require('knex')(knexfile[env]);
+debug('knex loaded');
 
 exports.Bookshelf = require('bookshelf');
+
 exports.Bookshelf.conexaoMain = require('bookshelf')(knex);
+debug('bookshelf loaded');
+exports.Bookshelf.conexaoMain.plugin('registry');
