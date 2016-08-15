0dc134e84d11167adb2144e19d1746f523afe54a
Flavio de Sousa <fl@viodesousa.com>
2016-08-15 23:48:14 +0200

    model conta._cadastrar

--- models/models/conta.js
+++ models/models/conta.js
@@ -1,10 +1,19 @@
-var Bookshelf = require('bookshelf').conexaoMain;
+'use strict';
+
+var _ = require('lodash');
+var AreaAzul = require('../../areaazul');
+var Bookshelf = AreaAzul.db.Bookshelf.conexaoMain;
 
 var Conta = Bookshelf.Model.extend({
-    tableName: 'conta',
-    idAttribute: 'id_conta'
-},{
-
+  tableName: 'conta',
+  idAttribute: 'id_conta'
+}, {
+  _cadastrar: function(conta, options) {
+    conta = _.merge(conta || {}, { saldo: 0, ativo: true });
+    var optionsInsert = _.merge({ method: 'insert' }, options);
+    return new Conta(conta)
+      .save(null, optionsInsert);
+  }
 });
 
 module.exports = Conta;
