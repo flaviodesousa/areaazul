fa5b3515c8b23f9bca351531ba97d63576eb401f
Flavio de Sousa <fl@viodesousa.com>
2016-08-21 15:28:46 +0200

    Registering all models and collections on main module loading

--- areaazul.js
+++ areaazul.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var debug = require('debug')('areaazul:main');
 var util = require('util');
 
 var BusinessException = function(message, details) {
@@ -8,9 +9,37 @@ var BusinessException = function(message, details) {
 };
 util.inherits(BusinessException, Error);
 
+var AuthenticationError = function(message, details) {
+  this.message = message;
+  this.details = details;
+};
+util.inherits(AuthenticationError, Error);
+
 module.exports.log = require('./configuration/logging');
 module.exports.db = require('./configuration/database');
-module.exports.models = require('./models/models');
-module.exports.collections = require('./models/collections');
+
+[
+  'estado', 'cidade', 'conta', 'pessoa', 'pessoa_fisica',
+  'pessoa_juridica', 'revendedor', 'contrato', 'funcionario', 'veiculo',
+  'configuracao', 'movimentacao_conta',
+  'usuario', 'usuario_revendedor', 'usuario_administrativo', 'usuario_fiscal',
+  'usuario_has_veiculo', 'ativacao', 'fiscalizacao'
+].forEach(model => {
+  debug('Registrando modelo ' + model);
+  require('./models/models/' + model);
+});
+
+[
+  'ativacoes', 'cidades', 'contas', 'contratos', 'estados', 'fiscalizacoes',
+  'funcionarios', 'movimentacoes_contas', 'pessoas', 'pessoas_fisicas',
+  'pessoas_juridicas', 'revendedores', 'usuarios', 'usuarios_have_veiculos',
+  'usuarios_revendedores', 'usuarios_fiscais', 'veiculos'
+].forEach(collection => {
+  debug('Registrando coleção ' + collection);
+  require('./models/collections/' + collection);
+});
+
 module.exports.BusinessException = BusinessException;
-module.exports.log.info('areaazul initialized');
+module.exports.AuthenticationError = AuthenticationError;
+
+module.exports.log.info('AreaAzul configurada');
