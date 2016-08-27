f4843de120b6a4d2550332ced71c2022a143e908
Flavio de Sousa <fl@viodesousa.com>
2016-08-27 16:40:50 +0200

    Relacionamentos explicitos para usuario e ativacao (PK)

--- migrations/20150422195630_ativacao.js
+++ migrations/20150422195630_ativacao.js
@@ -22,20 +22,21 @@ exports.up = function(knex) {
     });
   })
   .then(function() {
-    return knex.schema.createTable('ativacao_usuario_revendedor',
-      function(table) {
+    return knex.schema.createTable('ativacao_usuario_revendedor', table => {
       table.integer('ativacao_id').notNullable()
         .references('id').inTable('ativacao');
       table.integer('usuario_revendedor_id').notNullable()
         .references('id').inTable('usuario_revendedor');
+      table.primary(['ativacao_id', 'usuario_revendedor_id']);
     });
   })
   .then(function() {
-    return knex.schema.createTable('ativacao_usuario_fiscal', function(table) {
+    return knex.schema.createTable('ativacao_usuario_fiscal', table => {
       table.integer('ativacao_id').notNullable()
         .references('id').inTable('ativacao');
       table.integer('usuario_fiscal_id').notNullable()
         .references('id').inTable('usuario_fiscal');
+      table.primary(['ativacao_id', 'usuario_fiscal_id']);
     });
   });
 };
