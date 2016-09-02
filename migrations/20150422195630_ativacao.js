ad91521267037c3c875ae1d6cbf24bf54f9f4bc7
Flavio de Sousa <fl@viodesousa.com>
2016-09-02 21:20:10 +0200

    Cleaner multi-table creation syntax

--- migrations/20150422195630_ativacao.js
+++ migrations/20150422195630_ativacao.js
@@ -11,45 +11,30 @@ exports.up = function(knex) {
     table.string('tipo');
     table.integer('veiculo_id').notNullable()
       .references('id').inTable('veiculo');
-  })
-  .then(function() {
-    return knex.schema.createTable('ativacao_usuario', function(table) {
-      table.integer('ativacao_id').notNullable()
-        .references('id').inTable('ativacao');
-      table.integer('usuario_id').notNullable()
-        .references('id').inTable('usuario');
-      table.primary(['ativacao_id', 'usuario_id']);
-    });
-  })
-  .then(function() {
-    return knex.schema.createTable('ativacao_usuario_revendedor', table => {
-      table.integer('ativacao_id').notNullable()
-        .references('id').inTable('ativacao');
-      table.integer('usuario_revendedor_id').notNullable()
-        .references('id').inTable('usuario_revendedor');
-      table.primary(['ativacao_id', 'usuario_revendedor_id']);
-    });
-  })
-  .then(function() {
-    return knex.schema.createTable('ativacao_usuario_fiscal', table => {
-      table.integer('ativacao_id').notNullable()
-        .references('id').inTable('ativacao');
-      table.integer('usuario_fiscal_id').notNullable()
-        .references('id').inTable('usuario_fiscal');
-      table.primary(['ativacao_id', 'usuario_fiscal_id']);
-    });
+  }).createTable('ativacao_usuario', function(table) {
+    table.integer('ativacao_id').notNullable()
+      .references('id').inTable('ativacao');
+    table.integer('usuario_id').notNullable()
+      .references('id').inTable('usuario');
+    table.primary(['ativacao_id', 'usuario_id']);
+  }).createTable('ativacao_usuario_revendedor', table => {
+    table.integer('ativacao_id').notNullable()
+      .references('id').inTable('ativacao');
+    table.integer('usuario_revendedor_id').notNullable()
+      .references('id').inTable('usuario_revendedor');
+    table.primary(['ativacao_id', 'usuario_revendedor_id']);
+  }).createTable('ativacao_usuario_fiscal', table => {
+    table.integer('ativacao_id').notNullable()
+      .references('id').inTable('ativacao');
+    table.integer('usuario_fiscal_id').notNullable()
+      .references('id').inTable('usuario_fiscal');
+    table.primary(['ativacao_id', 'usuario_fiscal_id']);
   });
 };
 
 exports.down = function(knex) {
   return knex.schema.dropTableIfExists('ativacao_usuario_fiscal')
-    .then(function() {
-      return knex.schema.dropTableIfExists('ativacao_usuario_revendedor');
-    })
-    .then(function() {
-      return knex.schema.dropTableIfExists('ativacao_usuario');
-    })
-    .then(function() {
-      return knex.schema.dropTableIfExists('ativacao');
-    });
+    .dropTableIfExists('ativacao_usuario_revendedor')
+    .dropTableIfExists('ativacao_usuario')
+    .dropTableIfExists('ativacao');
 };
