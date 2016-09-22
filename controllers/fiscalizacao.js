ed827c394a757dc87b574c51d98916007c5940fd
Flavio de Sousa <fl@viodesousa.com>
2016-09-22 23:18:27 +0200

    controller fiscalizacao reestruturado

--- controllers/fiscalizacao.js
+++ controllers/fiscalizacao.js
@@ -1,41 +1,38 @@
 'use strict';
 
-module.exports = function () {
-  var AreaAzul = require('areaazul');
-  const Bookshelf = AreaAzul.db;
+const AreaAzul = require('areaazul');
+const Bookshelf = AreaAzul.db;
 
-  return {
-    registrar: function (req, res) {
-      Bookshelf.model('Fiscalizacao')
-        .cadastrar({
-          usuario_fiscal_id: req.user.id,
-          placa: req.body.placa,
-          latitude: req.body.latitude,
-          longitude: req.body.longitude
-        })
-        .then(function () {
-          res.status(200).end();
-        })
-        .catch(function (result) {
-          res.status(400).send('' + result);
-        });
-    },
-    listar: function (req, res) {
-      var params = {};
-      if (req.query.minutos) {
-        params.minutos = Number(req.query.minutos);
-      }
-      if (req.query.limite) {
-        params.limite = Number(req.query.limite);
-      }
-      Bookshelf.collection('Fiscalizacoes')
-        .listar(params)
-        .then(function (fiscalizacoes) {
-          res.send(fiscalizacoes.toJSON());
-        })
-        .catch(function (result) {
-          res.status(400).send('' + result);
-        });
-    }
-  };
+module.exports.registrar = function (req, res) {
+  Bookshelf.model('Fiscalizacao')
+    .cadastrar({
+      usuario_fiscal_id: req.user.id,
+      placa: req.body.placa,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude
+    })
+    .then(function () {
+      res.status(200).end();
+    })
+    .catch(function (result) {
+      res.status(400).send('' + result);
+    });
+};
+
+module.exports.listar = function (req, res) {
+  var params = {};
+  if (req.query.minutos) {
+    params.minutos = Number(req.query.minutos);
+  }
+  if (req.query.limite) {
+    params.limite = Number(req.query.limite);
+  }
+  Bookshelf.collection('Fiscalizacoes')
+    .listar(params)
+    .then(function (fiscalizacoes) {
+      res.send(fiscalizacoes.toJSON());
+    })
+    .catch(function (result) {
+      res.status(400).send('' + result);
+    });
 };
