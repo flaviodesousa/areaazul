e151b622223f9832ce6b4e278b7999a25f1908a0
Flavio de Sousa <fl@viodesousa.com>
2016-10-14 07:45:23 +0200

    Exportando fachadas

--- areaazul.js
+++ areaazul.js
@@ -20,6 +20,22 @@ require('./database');
 
 module.exports.BusinessException = BusinessException;
 module.exports.AuthenticationError = AuthenticationError;
+module.exports.facade = {
+  Ativacao: require('./facade/ativacao'),
+  Cidade: require('./facade/cidade'),
+  Configuracao: require('./facade/configuracao'),
+  Fiscalizacao: require('./facade/fiscalizacao'),
+  MovimentacaoDeConta: require('./facade/movimentacao_conta'),
+  PessoaFisica: require('./facade/pessoa_fisica'),
+  PessoaJuridica: require('./facade/pessoa_juridica'),
+  Revendedor: require('./facade/revendedor'),
+  Usuario: require('./facade/usuario'),
+  UsuarioAdministrativo: require('./facade/usuario_administrativo'),
+  UsuarioFiscal: require('./facade/usuario_fiscal'),
+  UsuarioHasVeiculo: require('./facade/usuario_has_veiculo'),
+  UsuarioRevendedor: require('./facade/usuario_revendedor'),
+  Veiculo: require('./facade/veiculo')
+};
 
 log.info('AreaAzul configurada');
 debug('AreaAzul iniciada');
