a278d2338d0cf29d7e923eeff243552b5ee9982e
Flavio de Sousa <fl@viodesousa.com>
2016-10-22 05:33:02 +0200

    Configuração: validação movida para model

--- facade/configuracao.js
+++ facade/configuracao.js
@@ -3,7 +3,6 @@
  */
 const Bookshelf = require('../database');
 const Configuracao = Bookshelf.model('Configuracao');
-const validator = require('validator');
 
 module.exports.getConfiguracaoTempo = function() {
   return [ {
@@ -21,78 +20,12 @@ module.exports.getConfiguracaoTempo = function() {
   } ];
 };
 
-module.exports.alterar = function(camposConfig) {
-  return new Configuracao()
-    .fetch()
-    .then(function(configuracao) {
-      const configuracoes = {
-        valor_ativacao_reais: camposConfig.valor_ativacao_reais,
-        tempo_tolerancia_minutos: camposConfig.tempo_tolerancia_minutos,
-        franquia_minutos: camposConfig.franquia_minutos,
-        ciclo_ativacao_minutos: camposConfig.ciclo_ativacao_minutos,
-        ciclo_fiscalizacao_minutos:
-          camposConfig.ciclo_fiscalizacao_minutos,
-        cidade_id: camposConfig.cidade_id
-      };
-      if (configuracao == null) {
-        return Configuracao
-          .forge(configuracoes)
-          .save();
-      }
-      return configuracao
-        .save(configuracoes, { method: 'update' }, { patch: true });
-    })
-    .then(configuracao => {
-      return configuracao.toJSON();
-    });
-};
+module.exports.alterar = camposConfig =>
+  Configuracao
+    ._alterar(camposConfig)
+    .then(configuracao => configuracao.toJSON());
 
-module.exports.buscar = function() {
-  return Configuracao
+module.exports.buscar = () =>
+  Configuracao
     ._buscar()
     .then(configuracao => configuracao.toJSON());
-};
-
-module.exports.validar = function(config) {
-  var message = [];
-
-  if (validator.isNull(config.valor_ativacao_reais)) {
-    message.push(
-      {
-        attribute: 'valor_ativacao',
-        problem: 'Campo valor de ativação é obrigatório!'
-      });
-  }
-  if (validator.isNull(config.tempo_tolerancia_minutos)) {
-    message.push(
-      {
-        attribute: 'tempo_tolerancia',
-        problem: 'Campo tempo de tolerancia é obrigatório!'
-      });
-  }
-
-  if (validator.isNull(config.franquia_minutos)) {
-    message.push(
-      {
-        attribute: 'franquia',
-        problem: 'Campo franquia é obrigatório!'
-      });
-  }
-  if (validator.isNull(config.ciclo_ativacao_minutos)) {
-    message.push(
-      {
-        attribute: 'ciclo_ativacao',
-        problem: 'Campo ciclo de ativacao é obrigatório!'
-      });
-  }
-
-  if (validator.isNull(config.ciclo_fiscalizacao_minutos)) {
-    message.push(
-      {
-        attribute: 'ciclo_fiscalizacao',
-        problem: 'Campo ciclo de fiscalizacao é obrigatório!'
-      });
-  }
-
-  return message;
-};
--- models/models/configuracao.js
+++ models/models/configuracao.js
@@ -1,12 +1,61 @@
 'use strict';
 
+const _ = require('lodash');
+const debug = require('debug')('areaazul:model:configuracao');
 const money = require('money-math');
+const validator = require('validator');
+const Promise = require('bluebird');
+
+const AreaAzul = require('../../areaazul');
 const Bookshelf = require('../../database');
 
 var Configuracao = Bookshelf.Model.extend(
 {
   tableName: 'configuracao'
 }, {
+  _camposValidos: function(config) {
+    var message = [];
+
+    if (validator.isNull(config.valor_ativacao_reais)) {
+      message.push(
+        {
+          attribute: 'valor_ativacao',
+          problem: 'Valor de ativação é obrigatório!'
+        });
+    }
+    if (validator.isNull(config.tempo_tolerancia_minutos)) {
+      message.push(
+        {
+          attribute: 'tempo_tolerancia',
+          problem: 'Tempo de tolerância é obrigatório!'
+        });
+    }
+
+    if (validator.isNull(config.franquia_minutos)) {
+      message.push(
+        {
+          attribute: 'franquia',
+          problem: 'Franquia é obrigatória!'
+        });
+    }
+    if (validator.isNull(config.ciclo_ativacao_minutos)) {
+      message.push(
+        {
+          attribute: 'ciclo_ativacao',
+          problem: 'Ciclo de ativaćão é obrigatório!'
+        });
+    }
+
+    if (validator.isNull(config.ciclo_fiscalizacao_minutos)) {
+      message.push(
+        {
+          attribute: 'ciclo_fiscalizacao',
+          problem: 'Ciclo de fiscalizaćão é obrigatório!'
+        });
+    }
+
+    return Promise.resolve(message);
+  },
   _buscar: () =>
     new Configuracao()
       .fetch({ require: true })
@@ -27,7 +76,37 @@ var Configuracao = Bookshelf.Model.extend(
           cidade_id: 1
         })
           .save()
-          .then(configuracao => configuracao.fetch()))
+          .then(configuracao => configuracao.fetch())),
+  _alterar: (camposConfig, options) => Configuracao
+    ._camposValidos(camposConfig)
+    .then(messages => {
+      if (messages && messages.length) {
+        debug('_alterar() configuração inválida');
+        throw new AreaAzul
+          .BusinessException(
+          'Não foi possível alterar configuração. Dados inválidos',
+          messages);
+      }
+      return new Configuracao().fetch();
+    })
+    .then(configuracao => {
+      const configuracoes = {
+        valor_ativacao_reais: camposConfig.valor_ativacao_reais,
+        tempo_tolerancia_minutos: camposConfig.tempo_tolerancia_minutos,
+        franquia_minutos: camposConfig.franquia_minutos,
+        ciclo_ativacao_minutos: camposConfig.ciclo_ativacao_minutos,
+        ciclo_fiscalizacao_minutos:
+        camposConfig.ciclo_fiscalizacao_minutos,
+        cidade_id: camposConfig.cidade_id
+      };
+      if (configuracao) {
+        return configuracao
+          .save(configuracoes,
+            _.merge({ method: 'update', patch: true }, options));
+      }
+      return new Configuracao(configuracoes)
+        .save(null, options);
+    })
 });
 Bookshelf.model('Configuracao', Configuracao);
 
