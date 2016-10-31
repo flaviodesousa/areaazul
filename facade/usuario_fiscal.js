515f113587e95c1745f943d56cef05c50c9cb944
Flavio de Sousa <fl@viodesousa.com>
2016-10-31 11:55:56 +0100

    Fachada UsuarioFiscal: lógica movida para Model

--- facade/usuario_fiscal.js
+++ facade/usuario_fiscal.js
@@ -1,104 +1,20 @@
-const _ = require('lodash');
-const bcrypt = require('bcrypt-then');
-const AreaAzul = require('../areaazul');
 const Bookshelf = require('../database');
 const log = require('../logging');
-
+const AreaazulUtils = require('areaazul-utils');
 const UsuarioFiscal = Bookshelf.model('UsuarioFiscal');
-const PessoaFisica = Bookshelf.model('PessoaFisica');
-const Conta = Bookshelf.model('Conta');
 
 module.exports.cadastrar = function(camposUsuarioFiscal) {
-  var conta;
-
+  log.info('UsuarioFiscal::cadastrar()', AreaazulUtils.semSenhas(camposUsuarioFiscal));
   return Bookshelf.transaction(function(t) {
-    var options = { transacting: t };
-    var optionsInsert = _.merge({ method: 'insert' }, options);
     return UsuarioFiscal
-      ._camposValidos(camposUsuarioFiscal, null, options)
-      .then(function(messages) {
-        if (messages.length) {
-          throw new AreaAzul
-            .BusinessException(
-            'Não foi possível cadastrar novo Usuário Fiscal.'
-            + ' Dados inválidos',
-            messages);
-        }
-      })
-      .then(function() {
-        return new UsuarioFiscal(
-          { login: camposUsuarioFiscal.login })
-          .fetch(_.merge({ require: true }, options));
-      })
-      .then(function(u) {
-        throw new AreaAzul.BusinessException(
-          'Alteração de usuário fiscal: ainda não suportada',
-          { usuario: u });
-      })
-      .catch(Bookshelf.NotFoundError, () => {
-        // Novo usuário fiscal
-        var pessoaFisica;
-        return new PessoaFisica({ cpf: camposUsuarioFiscal.cpf })
-          .fetch(_.merge({ require: true }, options))
-          .catch(Bookshelf.NotFoundError, () => {
-            return PessoaFisica
-              ._cadastrar(camposUsuarioFiscal, options);
-          })
-          .then(function(pf) {
-            pessoaFisica = pf;
-            return Conta._cadastrar(null, options);
-          })
-          .then(function(c) {
-            conta = c;
-            return bcrypt.hash(camposUsuarioFiscal.nova_senha);
-          })
-          .then(function(hash) {
-            return new UsuarioFiscal({
-              id: pessoaFisica.id,
-              login: camposUsuarioFiscal.login,
-              senha: hash,
-              conta_id: conta.id,
-              ativo: true
-            })
-              .save(null, optionsInsert);
-          })
-          .then(usuarioFiscal => {
-            return UsuarioFiscal
-              ._buscarPorId(usuarioFiscal.id, options);
-          })
-          .then(usuFis => usuFis.toJSON());
-      });
+      ._cadastrar(camposUsuarioFiscal, { transacting: t })
+      .then(usuFis => usuFis.toJSON());
   });
 };
 module.exports.autentico = function(login, senha) {
-  var usuarioFiscal;
-  return new UsuarioFiscal({ login: login })
-    .fetch()
-    .then(function(ur) {
-      usuarioFiscal = ur;
-      if (!usuarioFiscal) {
-        var err = new AreaAzul.AuthenticationError(
-          'Usuário fiscal: login inválido',
-          { login: login });
-        log.warn(err.message, err.details);
-        throw err;
-      }
-      return bcrypt.compare(senha, usuarioFiscal.get('senha'));
-    })
-    .then(function(valid) {
-      if (valid) {
-        return UsuarioFiscal
-          ._buscarPorId(usuarioFiscal.id, {})
-          .then(usuFis => usuFis.toJSON());
-      }
-      const err = new AreaAzul.AuthenticationError(
-        'Usuário fiscal: senha incorreta', {
-          login: login,
-          usuario: usuarioFiscal
-        });
-      log.warn(err.message, err.details);
-      throw err;
-    });
+  return UsuarioFiscal
+    ._autentico(login, senha)
+    .then(usuFis => usuFis.toJSON());
 };
 module.exports.buscarPorId = function(id) {
   return UsuarioFiscal
--- models/models/usuario_fiscal.js
+++ models/models/usuario_fiscal.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const _ = require('lodash');
+const bcrypt = require('bcrypt-then');
 const log = require('../../logging');
 const AreaAzul = require('../../areaazul');
 const Bookshelf = require('../../database');
+const PessoaFisica = Bookshelf.model('PessoaFisica');
+const Conta = Bookshelf.model('Conta');
 const UsuarioHelper = require('../../helpers/usuario_helper');
 
 var UsuarioFiscal = Bookshelf.Model.extend({
@@ -12,8 +15,93 @@ var UsuarioFiscal = Bookshelf.Model.extend({
     return this.hasOne('PessoaFisica', 'id');
   }
 }, {
-  _camposValidos: function(
-    camposUsuarioFiscal, usuarioFiscal, options) {
+  _cadastrar: (camposUsuarioFiscal, options) => {
+    var conta;
+    var optionsInsert = _.merge({ method: 'insert' }, options);
+    return UsuarioFiscal
+      ._camposValidos(camposUsuarioFiscal, null, options)
+      .then(function(messages) {
+        if (messages.length) {
+          throw new AreaAzul
+            .BusinessException(
+            'Não foi possível cadastrar novo Usuário Fiscal.'
+            + ' Dados inválidos',
+            messages);
+        }
+      })
+      .then(function() {
+        return new UsuarioFiscal(
+          { login: camposUsuarioFiscal.login })
+          .fetch(_.merge({ require: true }, options));
+      })
+      .then(function(u) {
+        throw new AreaAzul.BusinessException(
+          'Alteração de usuário fiscal: ainda não suportada',
+          { usuario: u });
+      })
+      .catch(Bookshelf.NotFoundError, () => {
+        // Novo usuário fiscal
+        var pessoaFisica;
+        return new PessoaFisica({ cpf: camposUsuarioFiscal.cpf })
+          .fetch(_.merge({ require: true }, options))
+          .catch(Bookshelf.NotFoundError, () => {
+            return PessoaFisica
+              ._cadastrar(camposUsuarioFiscal, options);
+          })
+          .then(function(pf) {
+            pessoaFisica = pf;
+            return Conta._cadastrar(null, options);
+          })
+          .then(function(c) {
+            conta = c;
+            return bcrypt.hash(camposUsuarioFiscal.nova_senha);
+          })
+          .then(function(hash) {
+            return new UsuarioFiscal({
+              id: pessoaFisica.id,
+              login: camposUsuarioFiscal.login,
+              senha: hash,
+              conta_id: conta.id,
+              ativo: true
+            })
+              .save(null, optionsInsert);
+          })
+          .then(usuarioFiscal => {
+            return UsuarioFiscal
+              ._buscarPorId(usuarioFiscal.id, options);
+          });
+      });
+  },
+  _autentico: (login, senha) => {
+    var usuarioFiscal;
+    return new UsuarioFiscal({ login: login })
+      .fetch()
+      .then(function(ur) {
+        usuarioFiscal = ur;
+        if (!usuarioFiscal) {
+          var err = new AreaAzul.AuthenticationError(
+            'Usuário fiscal: login inválido',
+            { login: login });
+          log.warn(err.message, err.details);
+          throw err;
+        }
+        return bcrypt.compare(senha, usuarioFiscal.get('senha'));
+      })
+      .then(function(valid) {
+        if (valid) {
+          return UsuarioFiscal
+            ._buscarPorId(usuarioFiscal.id, {});
+        }
+        const err = new AreaAzul.AuthenticationError(
+          'Usuário fiscal: senha incorreta', {
+            login: login,
+            usuario: usuarioFiscal
+          });
+        log.warn(err.message, err.details);
+        throw err;
+      });
+  },
+  _camposValidos: function(camposUsuarioFiscal, usuarioFiscal, options) {
     var messages = [];
 
     return UsuarioHelper
