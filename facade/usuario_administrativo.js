ae87b450e30d76b62f6ae0e16a975e909342623e
Flavio de Sousa <fl@viodesousa.com>
2016-10-31 11:29:40 +0100

    Fachada UsuarioAdministrativo: lógica movida para Model

--- facade/usuario_administrativo.js
+++ facade/usuario_administrativo.js
@@ -1,111 +1,20 @@
-const _ = require('lodash');
-const bcrypt = require('bcrypt-then');
-const log = require('../logging');
-const AreaAzul = require('../areaazul');
 const Bookshelf = require('../database');
-const PessoaFisica = Bookshelf.model('PessoaFisica');
 const UsuarioAdministrativo = Bookshelf.model('UsuarioAdministrativo');
 
 module.exports.cadastrar = function(camposUsuarioAdministrativo) {
-  var pessoaFisica;
-
-  return Bookshelf.transaction(function(t) {
-    var options = { transacting: t };
-    var optionsInsert = _.merge({ method: 'insert' }, options);
-    return UsuarioAdministrativo
-      ._camposValidos(camposUsuarioAdministrativo, null, options)
-      .then(function(messages) {
-        if (messages.length) {
-          throw new AreaAzul
-            .BusinessException(
-            'Não foi possível cadastrar novo Usuário Administrativo.'
-            + ' Dados inválidos',
-            messages);
-        }
-      })
-      .then(function() {
-        return new UsuarioAdministrativo(
-          { login: camposUsuarioAdministrativo.login })
-          .fetch(_.merge({ require: true }, options));
-      })
-      .then(function(u) {
-        throw new AreaAzul.BusinessException(
-          'Usuário administrativo: alteração ainda não suportada',
-          { usuarioAdministrativo: u }
-        );
-      })
-      .catch(Bookshelf.NotFoundError, () => {
-        return new PessoaFisica({ cpf: camposUsuarioAdministrativo.cpf })
-          .fetch(options);
-      })
-      .then(function(pf) {
-        if (!pf) {
-          return PessoaFisica._cadastrar(
-            camposUsuarioAdministrativo, options);
-        }
-        return pf;
-      })
-      .then(function(pf) {
-        pessoaFisica = pf;
-        return bcrypt.hash(camposUsuarioAdministrativo.nova_senha);
-      })
-      .then(function(hash) {
-        return new UsuarioAdministrativo({
-          id: pessoaFisica.id,
-          login: camposUsuarioAdministrativo.login,
-          senha: hash,
-          autorizacao: camposUsuarioAdministrativo.autorizacao,
-          ativo: true
-        })
-          .save(null, optionsInsert);
-      })
-      .then(usuarioAdministrativo => {
-        return UsuarioAdministrativo
-          ._buscarPorId(usuarioAdministrativo.id, options);
-      })
-      .then(usuarioAdministrativo => {
-        return usuarioAdministrativo.toJSON();
-      });
-  });
+  return Bookshelf.transaction(t =>
+    UsuarioAdministrativo
+      ._cadastrar(camposUsuarioAdministrativo, { transacting: t }))
+    .then(usuarioAdministrativo => usuarioAdministrativo.toJSON());
 };
-
 module.exports.autentico = function(login, senha) {
-  var usuarioAdministrativo;
-  return new UsuarioAdministrativo({ login: login })
-    .fetch()
-    .then(function(ur) {
-      usuarioAdministrativo = ur;
-      if (!usuarioAdministrativo) {
-        var err = new AreaAzul.AuthenticationError(
-          'Usuário administrativo: login inválido',
-          { login: login });
-        log.warn(err.message, err.details);
-        throw err;
-      }
-      return bcrypt.compare(senha, usuarioAdministrativo.get('senha'));
-    })
-    .then(function(valid) {
-      if (valid) {
-        return UsuarioAdministrativo
-          ._buscarPorId(usuarioAdministrativo.id, null)
-          .then(usuAdm => {
-            return usuAdm.toJSON();
-          });
-      }
-      const err = new AreaAzul.AuthenticationError(
-        'Usuário administrativo: senha incorreta', {
-          login: login,
-          usuario: usuarioAdministrativo
-        });
-      log.warn(err.message, err.details);
-      throw err;
-    });
+  return UsuarioAdministrativo
+    ._autentico(login, senha)
+    .then(usuAdm => usuAdm.toJSON());
 };
 
 module.exports.buscarPorId = function(id) {
   return UsuarioAdministrativo
     ._buscarPorId(id, null)
-    .then(usuAdm => {
-      return usuAdm.toJSON();
-    });
+    .then(usuAdm => usuAdm.toJSON());
 };
--- models/models/usuario_administrativo.js
+++ models/models/usuario_administrativo.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const _ = require('lodash');
-const Bookshelf = require('../../database');
-const AreaAzul = require('../../areaazul');
-const UsuarioHelper = require('../../helpers/usuario_helper');
-
+const bcrypt = require('bcrypt-then');
 const log = require('../../logging');
+const Bookshelf = require('../../database');
+
+const AreaAzul = require('../../areaazul');
+const PessoaFisica = Bookshelf.model('PessoaFisica');
+
+const UsuarioHelper = require('../../helpers/usuario_helper');
 
 var UsuarioAdministrativo = Bookshelf.Model.extend({
   tableName: 'usuario_administrativo',
@@ -13,6 +16,90 @@ var UsuarioAdministrativo = Bookshelf.Model.extend({
     return this.hasOne('PessoaFisica', 'id');
   }
 }, {
+  _cadastrar: (camposUsuarioAdministrativo, options) => {
+    var pessoaFisica;
+    var optionsInsert = _.merge({ method: 'insert' }, options);
+    return UsuarioAdministrativo
+      ._camposValidos(camposUsuarioAdministrativo, null, options)
+      .then(function(messages) {
+        if (messages.length) {
+          throw new AreaAzul
+            .BusinessException(
+            'Não foi possível cadastrar novo Usuário Administrativo.'
+            + ' Dados inválidos',
+            messages);
+        }
+      })
+      .then(function() {
+        return new UsuarioAdministrativo(
+          { login: camposUsuarioAdministrativo.login })
+          .fetch(_.merge({ require: true }, options));
+      })
+      .then(function(u) {
+        throw new AreaAzul.BusinessException(
+          'Usuário administrativo: alteração ainda não suportada',
+          { usuarioAdministrativo: u }
+        );
+      })
+      .catch(Bookshelf.NotFoundError, () => {
+        return new PessoaFisica({ cpf: camposUsuarioAdministrativo.cpf })
+          .fetch(options);
+      })
+      .then(function(pf) {
+        if (!pf) {
+          return PessoaFisica._cadastrar(
+            camposUsuarioAdministrativo, options);
+        }
+        return pf;
+      })
+      .then(function(pf) {
+        pessoaFisica = pf;
+        return bcrypt.hash(camposUsuarioAdministrativo.nova_senha);
+      })
+      .then(function(hash) {
+        return new UsuarioAdministrativo({
+          id: pessoaFisica.id,
+          login: camposUsuarioAdministrativo.login,
+          senha: hash,
+          autorizacao: camposUsuarioAdministrativo.autorizacao,
+          ativo: true
+        })
+          .save(null, optionsInsert);
+      })
+      .then(usuarioAdministrativo => {
+        return UsuarioAdministrativo
+          ._buscarPorId(usuarioAdministrativo.id, options);
+      });
+  },
+  _autentico: (login, senha) => {
+    var usuarioAdministrativo;
+    return new UsuarioAdministrativo({ login: login })
+      .fetch()
+      .then(function(ur) {
+        usuarioAdministrativo = ur;
+        if (!usuarioAdministrativo) {
+          var err = new AreaAzul.AuthenticationError(
+            'Usuário administrativo: login inválido',
+            { login: login });
+          log.warn(err.message, err.details);
+          throw err;
+        }
+        return bcrypt.compare(senha, usuarioAdministrativo.get('senha'));
+      })
+      .then(function(valid) {
+        if (valid) {
+          return UsuarioAdministrativo
+            ._buscarPorId(usuarioAdministrativo.id, null);
+        }
+        const err = new AreaAzul.AuthenticationError(
+          'Usuário administrativo: senha incorreta', {
+            login: login,
+            usuario: usuarioAdministrativo
+          });
+        log.warn(err.message, err.details);
+        throw err;
+      });
+  },
   _camposValidos: function(camposUsuAdm, usuarioAdministrativo, options) {
     var messages = [];
 
