0c97f84beee4c4cf6556b2cfc0ff10783a4752b1
Flávio de Sousa <flavio.desousa@rohde-schwarz.com>
2016-11-09 16:42:22 +0100

    Tabela Cidade: coluna 'nome_busca' adicionada

--- migrations/20150422195549_cidade.js
+++ migrations/20150422195549_cidade.js
@@ -4,8 +4,11 @@ exports.up = function(knex) {
   return knex.schema.createTable('cidade', function(table) {
     table.increments('id').primary();
     table.string('nome').notNullable();
+    table.string('nome_busca').notNullable()
+      .comment('Nome da cidade sem acentos, para buscas');
     table.integer('estado_id').notNullable()
       .references('id').inTable('estado');
+    table.index('nome_busca');
   });
 };
 
