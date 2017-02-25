b38ce16b392acdddcb4b29541e06f00053e2df53
Flavio de Sousa <fl@viodesousa.com>
2017-02-25 21:51:49 +0100

    Opcao para binding geral de host

--- bin/areaazul-api-web.js
+++ bin/areaazul-api-web.js
@@ -5,9 +5,20 @@
 const debug = require('debug')('areaazul');
 const app = require('../app');
 
-app.set('port', process.env.AREAAZUL_API_PORT || 18300);
-app.set('host', process.env.AREAAZUL_API_HOST || 'localhost');
+let server;
+let port = process.env.AREAAZUL_API_PORT || 18300;
+let host;
+if (!process.env.AREAAZUL_API_NOHOST) {
+  host = process.env.AREAAZUL_API_HOST || 'localhost';
+}
 
-const server = app.listen(app.get('port'), app.get('host'), function() {
+function onListening() {
   debug('Express server listening on port ' + server.address().port);
-});
+}
+
+if (host) {
+  server = app.listen(port, host, onListening);
+} else {
+  server = app.listen(port, onListening);
+}
+
