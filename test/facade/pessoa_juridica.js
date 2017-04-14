76acea04334ea0ad179eeaa553ed644ffcb41ae9
Flavio de Sousa <fl@viodesousa.com>
2017-04-14 19:38:01 +0200

    Maior cobertura de testes para PJ

--- test/facade/pessoa_juridica.js
+++ test/facade/pessoa_juridica.js
@@ -36,6 +36,132 @@ describe('facade PessoaJuridica', function() {
 
   describe('cadastrar()', function() {
 
+    it('falha sem nome', function(done) {
+      PessoaJuridica.cadastrar({
+        telefone: '00 0000 0000',
+        email: 'email@teste.teste',
+        cnpj: cnpjTeste,
+        nome_fantasia: 'PJ preexistente',
+        razao_social: 'razao social teste',
+        contato: 'contato-teste'
+      })
+        .then(function() {
+          done(new Error('deveria ter falhado'));
+        })
+        .catch(function(e) {
+          should.exist(e);
+          done();
+        });
+    });
+
+    it('falha sem telefone', function(done) {
+      PessoaJuridica.cadastrar({
+        nome: 'nome-preexistente',
+        email: 'email@teste.teste',
+        cnpj: cnpjTeste,
+        nome_fantasia: 'PJ preexistente',
+        razao_social: 'razao social teste',
+        contato: 'contato-teste'
+      })
+        .then(function() {
+          done(new Error('deveria ter falhado'));
+        })
+        .catch(function(e) {
+          should.exist(e);
+          done();
+        });
+    });
+
+    it('falha sem email', function(done) {
+      PessoaJuridica.cadastrar({
+        nome: 'nome-preexistente',
+        telefone: '00 0000 0000',
+        cnpj: cnpjTeste,
+        nome_fantasia: 'PJ preexistente',
+        razao_social: 'razao social teste',
+        contato: 'contato-teste'
+      })
+        .then(function() {
+          done(new Error('deveria ter falhado'));
+        })
+        .catch(function(e) {
+          should.exist(e);
+          done();
+        });
+    });
+
+    it('falha sem cnpj', function(done) {
+      PessoaJuridica.cadastrar({
+        nome: 'nome-preexistente',
+        telefone: '00 0000 0000',
+        email: 'email@teste.teste',
+        nome_fantasia: 'PJ preexistente',
+        razao_social: 'razao social teste',
+        contato: 'contato-teste'
+      })
+        .then(function() {
+          done(new Error('deveria ter falhado'));
+        })
+        .catch(function(e) {
+          should.exist(e);
+          done();
+        });
+    });
+
+    it('falha sem nome_fantasia', function(done) {
+      PessoaJuridica.cadastrar({
+        nome: 'nome-preexistente',
+        telefone: '00 0000 0000',
+        email: 'email@teste.teste',
+        cnpj: cnpjTeste,
+        razao_social: 'razao social teste',
+        contato: 'contato-teste'
+      })
+        .then(function() {
+          done(new Error('deveria ter falhado'));
+        })
+        .catch(function(e) {
+          should.exist(e);
+          done();
+        });
+    });
+
+    it('falha sem razao_social', function(done) {
+      PessoaJuridica.cadastrar({
+        nome: 'nome-preexistente',
+        telefone: '00 0000 0000',
+        email: 'email@teste.teste',
+        cnpj: cnpjTeste,
+        nome_fantasia: 'PJ preexistente',
+        contato: 'contato-teste'
+      })
+        .then(function() {
+          done(new Error('deveria ter falhado'));
+        })
+        .catch(function(e) {
+          should.exist(e);
+          done();
+        });
+    });
+
+    it('falha sem contato', function(done) {
+      PessoaJuridica.cadastrar({
+        nome: 'nome-preexistente',
+        telefone: '00 0000 0000',
+        email: 'email@teste.teste',
+        cnpj: cnpjTeste,
+        nome_fantasia: 'PJ preexistente',
+        razao_social: 'razao social teste'
+      })
+        .then(function() {
+          done(new Error('deveria ter falhado'));
+        })
+        .catch(function(e) {
+          should.exist(e);
+          done();
+        });
+    });
+
     it('funciona!', function(done) {
       PessoaJuridica.cadastrar({
         nome: 'nome-preexistente',
