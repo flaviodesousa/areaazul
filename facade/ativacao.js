9ad8f065bcdbabcd0b0077664b50d7c658da223d
Flávio de Sousa <flavio.desousa@rohde-schwarz.com>
2017-05-02 18:33:24 +0200

    Ativação: listarAtivacoes não usando transação

--- facade/ativacao.js
+++ facade/ativacao.js
@@ -11,28 +11,29 @@ module.exports.ativar = function(camposAtivacao) {
   log.info('ativar', { ativacao: camposAtivacao });
   return Bookshelf.transaction(t =>
     Ativacao
-      ._ativar(camposAtivacao, { transacting: t })
-      .then(ativacao => ativacao.toJSON()));
+      ._ativar(camposAtivacao, { transacting: t }))
+    .then(ativacao => ativacao.toJSON());
 };
 
 module.exports.desativar = function(desativacao) {
   log.info('desativar', desativacao);
   return Bookshelf.transaction(t =>
     Ativacao
-      ._desativar(desativacao, { transacting: t })
-      .then(desativacao => desativacao.toJSON()));
+      ._desativar(desativacao, { transacting: t }))
+    .then(desativacao => desativacao.toJSON());
 };
 
 module.exports.ativarPorRevenda = function(ativacao) {
   log.info('ativarPorRevenda()', ativacao);
   return Bookshelf.transaction(t =>
     Ativacao
-      ._ativarPorRevenda(ativacao, { transacting: t })
-      .then(ativacao => ativacao.toJSON()));
+      ._ativarPorRevenda(ativacao, { transacting: t }))
+    .then(ativacao => ativacao.toJSON());
 };
 
 module.exports.listarAtivacoes = function() {
-  return Ativacoes
-    ._listarAtivacoes()
+  return Bookshelf.transaction(t =>
+    Ativacoes
+      ._listarAtivacoes())
     .then(ativacoes => ativacoes.toJSON());
 };
